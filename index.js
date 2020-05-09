e36063d8e1ed843ef8e0e212b45b5c3d7eedaf9a
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-05-09 18:46:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -508,7 +508,7 @@ const command = args.shift().toLowerCase();
 		const taggedUser = msg.mentions.members.first();
 			const embed = new Discord.MessageEmbed()
 			.setColor('#FF69B4')
-			.setDescription('hello ' + msg.guild.members.get(msg.author.id) + ' and '+ taggedUser.displayName + '!')
+			.setDescription('hello ' + guild.member(msg.author) + ' and '+ taggedUser.displayName + '!')
 			msg.channel.send(embed);
 	}
  } 
