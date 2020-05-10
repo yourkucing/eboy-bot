b903c4576b445203dac74dd310f78cce9d5331f8
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-05-10 20:53:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -698,7 +698,10 @@ const command = args.shift().toLowerCase();
 		eboys.push({ id: msg.author.id, tantrum: 1 });
 	}
 	
-	fs.writeFile('./datacount.json', JSON.stringify(eboys, null, 2) , 'utf-8');
+	fs.writeFile('./datacount.json', JSON.stringify(eboys, null, 2) , function (err) {
+	  if (err) throw err;
+	  console.log('Saved!');
+	});
 	
         for (var i in eboys) {
           if (eboys[i].id === msg.author.id) {
