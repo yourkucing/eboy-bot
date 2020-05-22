3a7a01bfa130c5f01a54e9aff112f565214279a3
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-05-22 17:48:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1955,12 +1955,14 @@ else {
 			return msg.channel.send(`You can't duel yourself, ${msg.author}, unless you can split yourself into two?`);
 		}
 		else {
-			const player1 = msg.author;
-			const player2 = taggedUser;
+			const player1 = msg.guild.members.cache.get(msg.author.id).displayName;
+			const player2 = taggedUser.displayName;
 			msg.channel.send(`${taggedUser}, ${msg.guild.members.cache.get(msg.author.id).displayName} wants to duel you. Do you accept? (Reply yes or no.)`);
 			msg.channel.awaitMessages(m => m.author.id == taggedUser.id,
 					{max: 1, time: 10000}).then(collected => {
 							if (collected.first().content.toLowerCase() == 'yes') {
+								var p1points = 100;
+								var p2points = 100;
 								msg.channel.send("Let the battle begin!");
 							}
 
@@ -1970,6 +1972,150 @@ else {
 					}).catch(() => {
 							msg.channel.send('No reply after 10 seconds, duel is cancelled!');
 					});
+					
+			const results = [
+				{
+					xp: -10,
+					message: "Herbifors!"
+				},
+				{
+					xp: -20,
+					message: "Incendio!" 
+				},
+				{
+					xp: -10,
+					message: "Locomotor Mortis!"
+				},
+				{
+					xp: -10,
+					message: "Tarantallegra!"
+				},
+				{
+					xp: -5,
+					message: "Toe Biter!"
+				},
+				{
+					xp: -10,
+					message: "Waddiwasi!"
+				},
+				{
+					xp: -5,
+					message: "Bubble-producing Spell!"
+				},
+				{
+					xp: +20,
+					message: "Drink healing potion uwuuuuu :3"
+				},
+				{
+					xp: -10,
+					message: "Diminuendo!"
+				},
+				{
+					xp: -5,
+					message: "Babbling Curse!"
+				},
+				{
+					xp: -20,
+					message: "Engorgio Skullus!"
+				},
+				{
+					xp: -5,
+					message: "Mimble Wimble!"
+				},
+				{
+					xp: -10,
+					message: "Rictusempra!"
+				},
+				{
+					xp: -30,
+					message: "Slugulus Eructo!"
+				},
+				{
+					xp: -5,
+					message: "Christmas Decorations Spell!"
+				},
+				{
+					xp: -10,
+					message: "Ectomatic!"
+				},
+				{
+					xp: -20,
+					message: "Sardine Hex!"
+				},
+				{
+					xp: -10,
+					message: "Webbed Spell!"
+				},
+				{
+					xp: -10,
+					message: "Stretching Jinx!"
+				},
+				{
+					xp: -5,
+					message: "Cheering Charm!"
+				},
+				{
+					xp: -30,
+					message: "Glacius!"
+				},
+				{
+					xp: -10,
+					message: "Sponge-Knees!"
+				},
+				{
+					xp: -5,
+					message: "Steleus!"
+				},			
+				{
+					xp: -10,
+					message: "Calvorio!"
+				},
+				{
+					xp: -40,
+					message: "Duro!"
+				},
+				{
+					xp: -20,
+					message: "Vomitare Virids!"
+				},
+				{
+					xp: -5,
+					message: "Densaugeo!"
+				},
+				{
+					xp: -10,
+					message: "Melofors!"
+				},
+				{
+					xp: -20,
+					message: "Cornflake skin spell!"
+				},
+				{
+					xp: -10,
+					message: "Piscifors!"
+				}
+			]
+			
+			if (p1points === 100 && p2points === 100) {
+				while (p1points != 0 || p2points != 0) {
+					const randomOption = results[Math.floor(Math.random() * results.length)];
+					msg.channel.send(`**${player1}** casted ${randomOption.message} (∩•̀ω•́)⊃-⋆`);
+					p2points = p2points + randomOption.xp;
+					msg.channel.send(`**${player1}**: ${p1points}, **${player2}**: ${p2points}`);
+					const randomOption2 = choices[Math.floor(Math.random() * choices.length)];
+					msg.channel.send(`**${player2}** casted ${randomOption2.message} ⋆-⊂(•̀ω•́∩)`);
+					p1points = p1points + randomOption2.xp;
+					msg.channel.send(`**${player1}**: ${p1points}, **${player2}**: ${p2points}`);
+				}
+				if (p1points === 0) {
+					msg.channel.send(`**${player1}** is DEFEATED. AAAAAAh! (ノﾟДﾟ)八(ﾟДﾟ)八(ﾟДﾟ)ﾉｨｪｰｨ！`);
+					msg.channel.send(`CONGRATULATIONS, ${player2}! (っ◕‿◕)っ :heart:`);
+				}
+				if (p2points === 0) {
+					msg.channel.send(`**${player2}** is DEFEATED. AAAAAAh! (ノﾟДﾟ)八(ﾟДﾟ)八(ﾟДﾟ)ﾉｨｪｰｨ！`);
+					msg.channel.send(`CONGRATULATIONS, ${player1}! (っ◕‿◕)っ :heart:`);
+				}
+			}
 		}
 	}
  }
