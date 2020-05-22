6a7167ccee5a9298a7a2fa33808669b202f2eef7
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-05-22 15:18:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1607,6 +1607,115 @@ else {
 	}
  }
  
+
+ if (command === 'love') {
+	if (!msg.mentions.users.size) {
+		return msg.channel.send(`You didn't provide any name, ${msg.author}! What a dumdum.`);
+	}
+	else {
+		var gifs = [
+			'https://media2.giphy.com/media/yc2pHdAoxVOrJ2m5Ha/200.gif',
+			'https://media3.giphy.com/media/hqTwf417EvUWeV0XcX/giphy.gif',
+			'https://media2.giphy.com/media/kG3kzEP6BCpyg0YNMi/giphy.gif',
+			'https://media.giphy.com/media/l4pTdcifPZLpDjL1e/giphy.gif',
+			'https://data.whicdn.com/images/293095869/original.gif'
+			]
+		const taggedUser = msg.mentions.members.first();
+		if (taggedUser.id === msg.author.id) {
+			
+        var author = msg.author.id;
+        var tagged = msg.author.id;
+        
+        if(eboys.some(eboy => eboy.id === author)){
+            for (var i in eboys) {
+              if (eboys[i].id === author) {
+                if ('love' in eboys[i]) {
+                      for (var j in eboys[i].love) {
+                      	if (tagged in eboys[i].love[j]) {
+                      		eboys[i].love[j][tagged] = eboys[i].love[j][tagged] + 1;
+                     }
+                     else {
+                     	eboys[i].love[j][tagged] = 1;
+                     }
+                     }
+                }
+                else {
+                	eboys[i].love = [];
+                	eboys[i].love.push({[tagged]: 1});
+                }
+              }
+                     }
+                     }
+            else {
+            	eboys.push({ id: author, 'love': [{[tagged]: 1}] });
+            }
+            
+        for (var i in eboys) {
+          if (eboys[i].id === author) {
+            for (var j in eboys[i].love) {
+            	var drag = eboys[i].love[j][tagged];	
+            }
+          }
+        }
+
+			const embed = new Discord.MessageEmbed()
+			.setColor('#FF69B4')
+			.setDescription(`I love myself uwu`)
+			.setImage(gifs[Math.floor(Math.random()*gifs.length)])
+			.setFooter(`${msg.guild.members.cache.get(msg.author.id).displayName} loves themselves ${love} times.`);
+			msg.channel.send(embed);
+		}
+		else {
+			
+        var author = msg.author.id;
+        var tagged = taggedUser.id;
+        
+        if(eboys.some(eboy => eboy.id === author)){
+            for (var i in eboys) {
+              if (eboys[i].id === author) {
+                if ('love' in eboys[i]) {
+                      for (var j in eboys[i].love) {
+                      	if (tagged in eboys[i].love[j]) {
+                      		eboys[i].love[j][tagged] = eboys[i].love[j][tagged] + 1;
+                     }
+                     else {
+                     	eboys[i].love[j][tagged] = 1;
+                     }
+                     }
+                }
+                else {
+                	eboys[i].love = [];
+                	eboys[i].love.push({[tagged]: 1});
+                }
+              }
+                     }
+                     }
+            else {
+            	eboys.push({ id: author, 'love': [{[tagged]: 1}] });
+            }
+            
+        for (var i in eboys) {
+          if (eboys[i].id === author) {
+            for (var j in eboys[i].love) {
+            	var love = eboys[i].love[j][tagged];	
+            }
+          }
+        }
+
+
+			const embed = new Discord.MessageEmbed()
+			.setColor('#FF69B4')
+			.setDescription(`${msg.guild.members.cache.get(msg.author.id).displayName} loves you, **${taggedUser.displayName}**! I love you.`)
+			.setImage(gifs[Math.floor(Math.random()*gifs.length)])
+			.setFooter(`${msg.guild.members.cache.get(msg.author.id).displayName} loves ${taggedUser.displayName} ${love} times.`);
+			msg.channel.send(embed);
+
+		}
+	}
+ } 
+
+
+ 
  if (command === 'kill') {
 	if (!msg.mentions.users.size) {
 		return msg.channel.send(`You didn't provide any name, ${msg.author}! What a dumdum.`);
@@ -1722,7 +1831,24 @@ else {
 					msg.channel.send("Access Denied");
 				});
 	}
- } 
+ }
+ 
+
+/*  if (command === 'duel') {
+	if (!msg.mentions.users.size) {
+		return msg.channel.send(`You didn't provide any name, ${msg.author}! What a dumdum.`);
+	}
+	else {
+		const taggedUser = msg.mentions.members.first();
+		if (taggedUser.id === msg.author.id) {
+			return msg.channel.send(`You can't duel yourself, ${msg.author}, unless you can split yourself into two?`);
+		}
+		else {
+			
+		}
+	} */
+	
+
  
 //-----------------------------------------------------------------------------------------------------------------------------------------
  
