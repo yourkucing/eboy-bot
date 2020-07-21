a897dff6f7d84ed4f9d9ed475c3c5265cb091530
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-07-21 14:23:56 +0800

    changes

--- index.js
+++ index.js
@@ -160,49 +160,6 @@ const command = args.shift().toLowerCase();
  }
 
 
-
-/*  if (command === 'defenestrate') {
-	var gifs = [
-		'https://thumbs.gfycat.com/MistyDelectableKouprey-size_restricted.gif',
-		'https://i.pinimg.com/originals/51/58/88/515888d6d7fadfd3dd0fa312ca287a34.gif'
-	]
-	if (!msg.mentions.users.size) {
-		const words = args.join(' ');
-		if (words === "") {
-			return msg.channel.send(`You didn't provide any name, ${msg.author}! What a dumdum.`);
-		}
-		else {
-			const embed = new Discord.MessageEmbed()
-			.setColor('#FF69B4')
-			.setDescription(`You defenestrated **${words}**!`)
-			.setImage(gifs[Math.floor(Math.random()*gifs.length)])
-			.setFooter(`Yikes, byebye ${words}!`);
-			msg.channel.send(embed);
-		}
-	}
-	else {
-		const taggedUser = msg.mentions.members.first();
-		if (taggedUser.id === msg.author.id) {
-			
-			const embed = new Discord.MessageEmbed()
-			.setColor('#FF69B4')
-			.setDescription(`You defenestrated yourself!`)
-			.setImage(gifs[Math.floor(Math.random()*gifs.length)])
-			.setFooter(`Yikes, byebye ${taggedUser.displayName}!`);
-			msg.channel.send(embed);
-		}
-		else {
-			
-			const embed = new Discord.MessageEmbed()
-			.setColor('#FF69B4')
-			.setDescription(`You defenestrated **${taggedUser.displayName}**!`)
-			.setImage(gifs[Math.floor(Math.random()*gifs.length)])
-			.setFooter(`Yikes, byebye ${taggedUser.displayName}!`);
-			msg.channel.send(embed);
-		}
-	}
- } */
- 
  if (command === 'yeet') {
 	var gifs = [
 		'https://media1.tenor.com/images/42b71e0d0b5df489510e78cb4af77902/tenor.gif',
