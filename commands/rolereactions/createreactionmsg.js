1117674a28dae9fae4720da660d8a2ab98f02e4e
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-07-23 21:22:48 +0800

    changes

---
+++ commands/rolereactions/createreactionmsg.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+module.exports.run = async(client, msg, args) => {
+	if(args.split(/\s+/).length !== 1) {
+		let msg1 = await msg.channel.send(`You only need to provide one message ID, ${msg.author}! What a dumdum.`);
+	    await msg1.delete({ timeout: 3500 }).catch(err => console.log(err));
+	}
+	else {
+		try {
+			let fetchedMessage = await msg.channel.messages.fetch(args);
+		}
+		catch(err) {
+			console.log(err);
+			let msg1 =  await msg.channel.send(`Message ID was not even valid, dumdum D:`);
+			await msg1.delete({ timeout: 3500 }).catch(err => console.log(err));
+		}
+	}
+}
