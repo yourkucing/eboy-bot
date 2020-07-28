0a632bf1db6f3a457b01ba525cd2531e52530216
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-07-28 19:36:13 +0800

    Update createreactionmsg.js

--- commands/rolereactions/createreactionmsg.js
+++ commands/rolereactions/createreactionmsg.js
@@ -29,5 +29,7 @@ module.exports = {
                 await msg1.delete({ timeout: 3500 }).catch(err => console.log(err));
             }
         }
-    }
+    },
+    aliases: [],
+    description: 'Enables a message to listen to reactions to give roles.'
 }
