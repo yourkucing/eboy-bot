cfce73da2a161e0892cae6921835ae7707a34858
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-07-28 18:43:35 +0800

    Update createreactionmsg.js

--- commands/rolereactions/createreactionmsg.js
+++ commands/rolereactions/createreactionmsg.js
@@ -1,14 +1,25 @@
-const Discord = require('discord.js');
-const fs = require('fs').promises;
+const { MessageCollector } = require('discord.js');
+
+let msgCollectorFilter = (newMsg, originalMsg) => {
+    let isSameAuthor = newMsg.author.id === originalMsg.author.id;
+    let args = originalMsg.content.split(/,\s+/);
+}
 
 module.exports.run = async(client, msg, args) => {
-	if(args.split(/\s+/).length !== 1) {
+	if(args.slice().length !== 1) {
 		let msg1 = await msg.channel.send(`You only need to provide one message ID, ${msg.author}! What a dumdum.`);
 	    await msg1.delete({ timeout: 3500 }).catch(err => console.log(err));
 	}
 	else {
 		try {
 			let fetchedMessage = await msg.channel.messages.fetch(args);
+            if(fetchedMessage) {
+                await msg.channel.send("Please provide all of the emoji names with the role name.\neg: emojiname, rolename");
+                let collector = new MessageCollector(message.channel, msgCollectorFilter.bind(null, msg));
+                collector.on('collect', message => {
+                    console.log(`${message.content} was collected.`);
+                });
+            }
 		}
 		catch(err) {
 			console.log(err);
