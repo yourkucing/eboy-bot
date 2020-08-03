5a0fa97986bdfed8e28a700558fecd575d22341a
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-08-03 13:47:09 +0800

    Update axe.js

--- commands/axe.js
+++ commands/axe.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async(client, msg, args) => {
 		const embed = new Discord.MessageEmbed()
 		.setColor('#FF69B4')
-		.setDescription(`How about a hatchet instead, ${msg.guild.members.cache.get(msg.author.id).displayName}?`)
+		.setDescription(`How about a hatchet instead, **${msg.guild.members.cache.get(msg.author.id).displayName}**?`)
 		.setImage('https://media1.tenor.com/images/ad46605733978b0992b375c01e456905/tenor.gif')
 		.setFooter(`An axe seems too big for you.`);
 		msg.channel.send(embed);
