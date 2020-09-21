f2ed1ea13e37c4c3cdc835864c7bea2e1c4b4a2e
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-09-21 14:31:56 +0800

    Update sortinghat.js

--- commands/expressions/sortinghat.js
+++ commands/expressions/sortinghat.js
@@ -7,10 +7,9 @@ module.exports.run = async(client, msg, args) => {
         ':regional_indicator_h: :regional_indicator_u: :regional_indicator_f: :regional_indicator_f: :regional_indicator_l: :regional_indicator_e: :regional_indicator_p: :regional_indicator_u: :regional_indicator_f: :regional_indicator_f:',
         ':regional_indicator_s: :regional_indicator_l: :regional_indicator_y: :regional_indicator_t: :regional_indicator_h: :regional_indicator_e: :regional_indicator_r: :regional_indicator_i: :regional_indicator_n:'
 		]
-    randomhouse = Math.floor(Math.random()*houses.length)
     msg.channel.send(`*You might belong in Gryffindor,\nWhere dwell the brave at heart,\nTheir daring, nerve and chivalry\nSet Gryffindors apart;\nYou might belong in Hufflepuff,\nWhere they are just and loyal,\nThose patient Hufflepuffs are true\nAnd unafraid of toil;\nOr yet in wise old Ravenclaw,\nIf you've a steady mind,\nWhere those of wit and learning,\nWill always find their kind;\nOr perhaps in Slytherin\nYou'll make your real friends,\nThose cunning folk use any means\nTo achieve their ends.*\n\nYOU, my dear hooman being, belong in......`).then((sentMessage) => 
         setTimeout(function(){
-            sentMessage.edit(houses[Math.floor(Math.random()*randomhouse)]);
+            sentMessage.edit(houses[Math.floor(Math.random()*houses.length)]]);
         }, 4000)
     )
 }
