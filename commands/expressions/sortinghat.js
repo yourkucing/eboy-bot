fecf9547d70da365b5c248099d1299423fc86c40
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-09-21 14:14:15 +0800

    Update sortinghat.js

--- commands/expressions/sortinghat.js
+++ commands/expressions/sortinghat.js
@@ -9,7 +9,7 @@ module.exports.run = async(client, msg, args) => {
 		]
     msg.channel.send(`*You might belong in Gryffindor,\nWhere dwell the brave at heart,\nTheir daring, nerve and chivalry\nSet Gryffindors apart;\nYou might belong in Hufflepuff,\nWhere they are just and loyal,\nThose patient Hufflepuffs are true\nAnd unafraid of toil;\nOr yet in wise old Ravenclaw,\nIf you've a steady mind,\nWhere those of wit and learning,\nWill always find their kind;\nOr perhaps in Slytherin\nYou'll make your real friends,\nThose cunning folk use any means\nTo achieve their ends.*`).then((sentMessage) => 
         setTimeout(function(){
-            sentMessage.edit("You belong in Gryffindor!");
-        }, 1000)
+            sentMessage.edit(`You belong in **${houses[Math.floor(Math.random()*houses.length)]}**!`);
+        }, 5000)
     )
 }
