7b4a41cf98174490405d50c3e84301b8260dc523
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-09-29 16:34:35 +0800

    Update duel.js

--- commands/duel.js
+++ commands/duel.js
@@ -39,31 +39,33 @@ module.exports.run = async(client, msg, args) => {
                     while (p1points > 0 && p2points > 0) {
                         const randomOption = results[Math.floor(Math.random() * results.length)];
                         msg.channel.send(`**${player1}** casted ${randomOption.message} (∩•̀ω•́)⊃-⋆`);
-                        const randomOption3 = blockorhit[Math.floor(Math.random()*blockorhit.length)];
-                        if (randomOption3 === "block")
+                        const randomOption3 = blockorhit[Math.floor(Math.random() * blockorhit.length)];
+                        if (randomOption3 == "block")
                             {
                                 msg.channel.send(`**${player1}**'s spell was blocked by **${player2}**! :shield:`);
                             }
-                        else if (randomOption3 === "hit")
+                        else if (randomOption3 == "hit")
                             {
                                 msg.channel.send(`**${player1}**'s spell hit **${player2}**! BAM :boom:`);
                                 p2points = p2points - Number(randomOption.xp); 
                             }
                         msg.channel.send(`**${player1}**: ${p1points}, **${player2}**: ${p2points}`);
+                        
                         const randomOption2 = results[Math.floor(Math.random() * results.length)];
                         msg.channel.send(`**${player2}** casted ${randomOption2.message} ⋆-⊂(•̀ω•́∩)`);
-                        const randomOption3 = blockorhit[Math.floor(Math.random()*blockorhit.length)];
-                        if (randomOption3 === "block")
+                        const randomOption4 = blockorhit[Math.floor(Math.random() * blockorhit.length)];
+                        if (randomOption4 == "block")
                             {
                                 msg.channel.send(`**${player2}**'s spell was blocked by **${player1}**! :shield:`);
                             }
-                        else if (randomOption3 === "hit")
+                        else if (randomOption4 == "hit")
                             {
                                 msg.channel.send(`**${player2}**'s spell hit **${player1}**! BAM :boom:`);
                                p1points = p1points - Number(randomOption2.xp);
                             }
                         msg.channel.send(`**${player1}**: ${p1points}, **${player2}**: ${p2points}`);
                     }
+                    
                     if (p1points <= 0 && p2points > 0) {
                         msg.channel.send(`**${player1}** is DEFEATED. AAAAAAh! (ノﾟДﾟ)八(ﾟДﾟ)八(ﾟДﾟ)ﾉｨｪｰｨ！`);
                         msg.channel.send(`CONGRATULATIONS, ${player2}! (っ◕‿◕)っ :heart:`);
