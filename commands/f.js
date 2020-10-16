895121a532ca41fa78baeb2ff126768173f8c672
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-10-17 00:11:50 +0800

    Update f.js

--- commands/f.js
+++ commands/f.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
     const filter = (reaction, user) => {
-        return reaction.emoji.name === `:regional_indicator_f:` && user.id === message.author.id;
+        return reaction.emoji.name === `:regional_indicator_f:` && user.id === msg.author.id;
     };
 
-    const collector = message.createReactionCollector(filter, { time: 15000 });
+    const collector = msg.createReactionCollector(filter, { time: 15000 });
 
     collector.on('collect', (reaction, user) => {
         console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
