d2c17467f3132fbf7f9e1ea59e54d7f66e197ec4
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-10-17 20:44:39 +0800

    changes

--- commands/f.js
+++ commands/f.js
@@ -1,18 +1,22 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
-    return msg.channel.send(`Noa is a dumdum.`);
-    const filter = (reaction, user) => {
-        return reaction.emoji.name === `:regional_indicator_f:` && user.id === msg.author.id;
-    };
+    msg.channel.send(`Noa is a dumdum.`)
+    .then(async function(msg) {
+        await msg.react()    
 
-    const collector = msg.createReactionCollector(filter, { time: 15000 });
+        const filter = (reaction, user) => {
+            return reaction.emoji.name === ':regional_indicator_f:' && user.id === msg.author.id;
+        };
 
-    collector.on('collect', (reaction, user) => {
-        console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
-    });
+        const collector = msg.createReactionCollector(filter, { time: 15000 });
 
-    collector.on('end', collected => {
-        console.log(`Collected ${collected.size} items`);
+        collector.on('collect', (reaction, user) => {
+            console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
+        });
+
+        collector.on('end', collected => {
+            console.log(`Collected ${collected.size} items`);
+        });
     });
 }
