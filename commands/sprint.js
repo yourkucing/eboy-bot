7f2e9b8049b39fdb7c60c68d086d3680a96d4cf9
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-11-05 18:07:59 +0800

    Update sprint.js

--- commands/sprint.js
+++ commands/sprint.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
-    if (args === "role") {
+    if (args == "role") {
         member = msg.member
         role = msg.guild.roles.cache.find(x => x.id == "773848625989156879")
         member.roles.add(role)
