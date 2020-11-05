7eff775b6780f5a296ef90e48eeac6100e135bff
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-11-05 18:05:52 +0800

    Update sprint.js

--- commands/sprint.js
+++ commands/sprint.js
@@ -2,13 +2,20 @@ const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
     if (args === "role") {
-        member = msg.author
+        member = msg.member
         role = msg.guild.roles.cache.find(x => x.id == "773848625989156879")
         member.roles.add(role)
     }
-    time =  parseFloat(args) * 60 * 1000
-    msg.channel.send(`Writing sprint for ${time} minutes starts now!`)
-    setTimeout(() => {
-      msg.channel.send('Testing with this');
-    }, time);
+    else {
+        time =  parseFloat(args) * 60 * 1000
+        if (isNaN(time)) {
+            msg.channel.send(`Nani the fuck is "uwu sprint ${args}"?`)
+        }
+        else {
+            msg.channel.send(`Writing sprint for ${time} minutes starts now!`)
+            setTimeout(() => {
+              msg.channel.send('Testing with this');
+            }, time);
+        }
+    }
 }
