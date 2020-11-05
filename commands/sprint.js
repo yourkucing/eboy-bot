8d8299f1587c2c7c33dda2273e72e2783a3d3158
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-11-05 17:59:10 +0800

    Update sprint.js

--- commands/sprint.js
+++ commands/sprint.js
@@ -1,7 +1,13 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
-    time =  parseFloat(args) * 60 * 1000 
+    if (args === "role") {
+        member = msg.author
+        role = msg.guild.roles.cache.find(x => x.id == "773848625989156879")
+        member.roles.add(role)
+    }
+    time =  parseFloat(args) * 60 * 1000
+    msg.channel.send(`Writing sprint for ${time} minutes starts now!`)
     setTimeout(() => {
       msg.channel.send('Testing with this');
     }, time);
