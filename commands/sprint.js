c050c10f05ed48297d808dc4d8d6eb384d65842d
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-11-13 16:19:08 +0800

    changes

--- commands/sprint.js
+++ commands/sprint.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async(client, msg, args) => {
     if (args == "role") {
         member = msg.member
-        role = msg.guild.roles.cache.find(x => x.id == "773859813138497536")
+        role = msg.guild.roles.cache.find(x => x.id == "776722239533219870")
         member.roles.add(role)
         msg.channel.send(`hi ${msg.author.toString()} uwu! I have added you to the **sprinters** role uwu :3 NOW WRITE WRITE WRITE~`)
     }
@@ -13,9 +13,9 @@ module.exports.run = async(client, msg, args) => {
             msg.channel.send(`Nani the fuck is "uwu sprint ${args}"?`)
         }
         else {
-            msg.channel.send(`<@&773859813138497536>! Writing sprint for ${args} minutes starts now!`)
+            msg.channel.send(`<@&776722239533219870>! Writing sprint for ${args} minutes starts now!`)
             setTimeout(() => {
-              msg.channel.send(`<@&773859813138497536>! Writing sprint is up!`);
+              msg.channel.send(`<@&776722239533219870>! Writing sprint is up!`);
             }, time);
         }
     }
