d1632707ce487765a9bad2e606fb546966208e01
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-12-21 18:30:55 +0800

    Update choose.js

--- commands/choose.js
+++ commands/choose.js
@@ -1,5 +1,13 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
-    msg.channel.send(args);
+    const words = args.join(' ')
+    choice = words.substring(
+                words.lastIndexOf("[") + 1, 
+                words.lastIndexOf("]")
+            )
+    choice = choice.split(",")
+    n = Math.floor(Math.random() * choice.length)
+    msg.channel.send("`Result: " + choice[n] + "`");
+    
 }
