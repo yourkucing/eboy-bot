11f94b187d6ffd2c7b13bb72c2abd046adb336d4
Maryam Dinzly <thatdinzlygirl@gmail.com>
2020-12-21 18:02:14 +0800

    Update dice.js

--- commands/dice.js
+++ commands/dice.js
@@ -2,6 +2,23 @@ const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
     dice = args[0].split("d")
-    result = Math.floor(Math.random() * dice[1]) + 1
-    msg.channel.send("```\nResult: " + result + "\nDetails: " + args[0] + " (" + result + ")\n```");
+    if (dice[0] === "") {
+        result = Math.floor(Math.random() * dice[1]) + 1
+        msg.channel.send("```\nResult: " + result + "\nDetails: " + args[0] + " (" + result + ")\n```");
+    }
+    else if (parseInt(dice[0]) === 1) {
+        result = Math.floor(Math.random() * dice[1]) + 1
+        msg.channel.send("```\nResult: " + result + "\nDetails: " + args[0] + " (" + result + ")\n```");        
+    }
+    else if (parseInt(dice[0]) > 1) {
+        n = 0
+        L = []
+        while (n < parseInt(dice[0])) {
+            result = Math.floor(Math.random() * dice[1]) + 1
+            L.push(result)
+            n = n + 1
+        }
+        total = L.reduce((a, b) => a + b)
+        msg.channel.send("```\nResult: " + total + "\nDetails: " + args[0] + " (" + L + ")\n```");
+    }
 }
