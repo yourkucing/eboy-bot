1eab10e08812fd9e7934deea45b4bf51126ce84a
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-07 15:36:36 +0800

    Update timeout.js

--- commands/timeout.js
+++ commands/timeout.js
@@ -10,20 +10,24 @@ module.exports.run = async(client, msg, args) => {
         time = 15000
         if (!msg.guild.roles.cache.find(x => x.name == "Time Out Corner")){
             highest = msg.guild.roles.highest.position
+            bothighest = msg.guild.members.resolve(bot.user).roles.highest.position
             console.log(highest)
-            try {
-                await msg.guild.roles.create({
-                    data: {
-                      name: 'Time Out Corner',
-                      color: 'BLACK',
-                      hoist:  true,
-                      position: highest-1
-                    },
-                    reason: 'to time people out',
-                  }).then(r => taggedUser.roles.add(r))
-            } catch(err) {
-                console.log(err)
-            }
+            console.log(bothighest)
+            const timeout = await msg.guild.roles.create({
+                data: {
+                    name: 'Time Out Corner',
+                    color: 'BLACK',
+                    hoist:  true,
+                    position: bothighest-1
+                },
+                reason: 'to time people out',
+                }).catch((err) => console.error(err))
+
+            taggedUser.roles.add(timeout).then(
+                () => {
+                    msg.channel.send(`${taggedUser.displayName} has been timed out for 15 minutes (default time). Shame on you!`)
+                }
+            ).catch((err) => console.error(err))
         }
         else {
             taggedUser.roles.add(msg.guild.roles.cache.find(x => x.name == "Time Out Corner"), "");
