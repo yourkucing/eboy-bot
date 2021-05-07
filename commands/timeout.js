6b1db3941a384610e86277ef02511de05f3a0f7b
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-07 16:35:18 +0800

    Update timeout.js

--- commands/timeout.js
+++ commands/timeout.js
@@ -4,34 +4,56 @@ module.exports.run = async(client, msg, args) => {
     if (!msg.mentions.users.size) {
         msg.channel.send(`I'm not sure... who you wanted me to time out?`)
     }
-    else if (!args[1]) {
+    else {
         taggedUser = msg.mentions.members.first()
         msg.channel.send(`I will time ${taggedUser.displayName} out for the default time of 15 minutes. Uh-oh, what did you do? Well, see you later then!`)
         time = 15000
         if (!msg.guild.roles.cache.find(x => x.name == "Time Out Corner")){
+
             highest = msg.guild.roles.highest.position
             bothighest = msg.guild.me.roles.highest.position
             console.log(highest)
             console.log(bothighest)
-            const timeout = await msg.guild.roles.create({
-                data: {
-                    name: 'Time Out Corner',
-                    color: 'BLACK',
-                    hoist:  true,
-                    position: bothighest-1
-                },
-                reason: 'to time people out',
-                }).catch((err) => console.error(err))
-
-            taggedUser.roles.add(timeout).then(
-                () => {
-                    msg.channel.send(`${taggedUser.displayName} has been timed out for 15 minutes (default time). Shame on you!`)
+            msg.channel.send(`\`Since this is the first time you run this command, I would like to let you know what is expected uwu.
+            
+            Timeout Command:\ncommand syntax: uwu timeout <tagged user> time [time should be in this format: 1m (1 minute) or 5s (5 seconds) or 4h (4 hours)]
+            A role will be created and given to the person being timed out. This will last for the specified time. However, if no time is stated, they will be timed out for a default of 5mins.
+            
+            What happens when being timed out:\nThis is where it differs depending on (a) if your bot has the highest role (or at least higher than most roles other than the admin) and (b) if they don't.\nIf they don't have a higher role, the user will be assigned the Time Out Corner role and that's it.
+            
+            On the other hand, if the bot's role is one of the highest, the user will be assigned the Timed Out Corner role and they will be redirected to a Timeout Channel where they will only be able to chat in there for the designated timeout time.
+            
+            With that being said, would you like to change your bot's role position? (Just answer yes or no within the next 1 minute.)\``)
+            msg.channel.awaitMessages(m => m.author.id == taggedUser.id, {max: 1, time: 30000}).then(collected => { 
+                if (collected.first().content.toLowerCase() == 'yes') {
+                    msg.channel.send(`Alright, come back later!`)
+                } 
+                else if (collected.first().content.toLowerCase() == 'no') {
+                    msg.channel.send(`Just remember that if your bot has a lower role, only the Time Out role will be assigned, and that's it!`)
+                    const timeout = await msg.guild.roles.create({
+                        data: {
+                            name: 'Time Out Corner',
+                            color: 'BLACK',
+                            hoist:  true,
+                            position: bothighest-1
+                        },
+                        reason: 'to time people out',
+                        }).catch((err) => console.error(err))
+        
+                    taggedUser.roles.add(timeout).then(
+                        () => {
+                            msg.channel.send(`**${taggedUser.displayName}** has been timed out for 15 minutes (default time). Shame on you!`)
+                        }
+                    ).catch((err) => console.error(err))
                 }
-            ).catch((err) => console.error(err))
+            }).catch(collected => {
+                msg.channel.send('No reply after 1 minute, command is cancelled!');
+                });
+
+            
         }
         else {
             taggedUser.roles.add(msg.guild.roles.cache.find(x => x.name == "Time Out Corner"), "");
         }
-        
     }
 }
