c8e9a6939892e350330d560f9a9aefd4db45a0e1
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-07 17:20:34 +0800

    Update timeout.js

--- commands/timeout.js
+++ commands/timeout.js
@@ -30,7 +30,22 @@ module.exports.run = async(client, msg, args) => {
                     msg.channel.send(`Alright, come back later!`)
                 } 
                 else if (collected.first().content.toLowerCase() == 'no') {
-
+                    msg.channel.send(`Just remember that if your bot has a lower role, only the Time Out role will be assigned, and that's it!`)
+                    const timeout = msg.guild.roles.create({
+                        data: {
+                            name: 'Time Out Corner',
+                            color: 'BLACK',
+                            hoist:  true,
+                            position: bothighest-1
+                        },
+                        reason: 'to time people out',
+                        }).catch((err) => console.error(err))
+        
+                    taggedUser.roles.add(timeout).then(
+                        () => {
+                            msg.channel.send(`**${taggedUser.displayName}** has been timed out for 15 minutes (default time). Shame on you!`)
+                        }
+                    ).catch((err) => console.error(err))
                 }
                 else {
                     msg.channel.send(`Right... Okay, goodbye!`)
