de2aacded25186eec407ef3e0147a650dc1171eb
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-10 00:04:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,9 +36,24 @@ client.on('ready', () => {
 		}
 		const results = await timeoutModel.find(query)
 		for (const post of results) {
-			console.log(post)
+			userID = userID
+			guildID = guildID
+			channelID = channelID
+
+			const guild = await client.guilds.fetch(guildID)
+			if (!guild) {
+				continue
+			}
+			
+			const channel = await client.channels.cache.get(channelID)
+			if (!channel) {
+				continue
+			}
+			guild.members.cache.get(userID).roles.remove(result)
+			channel.send(`<@${userID}>, your timeout has ended!`)
 		}
 
+		await timeoutModel.deleteMany(query)
 		setTimeout(checkforTimeouts, 1000 * 10)
 	}
 	checkforTimeouts()
