9da5bad7b9fa8918026d094520add1d7463a4158
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-09 18:03:35 +0800

    Update timeout.js

--- commands/timeout.js
+++ commands/timeout.js
@@ -23,7 +23,7 @@ module.exports.run = async(client, msg, args) => {
                 bothighest = msg.guild.me.roles.highest.position
                 console.log(highest)
                 console.log(bothighest)
-                msg.channel.send(`\`\`\`Since this is the first time you run this command, I would like to let you know what is expected uwu.
+/*                 msg.channel.send(`\`\`\`Since this is the first time you run this command, I would like to let you know what is expected uwu.
                 
     Timeout Command:\ncommand syntax: uwu timeout <tagged user> time [time should be in this format: 1m (1 minute) or 5s (5 seconds) or 4h (4 hours)]
     A role will be created and given to the person being timed out. This will last for the specified time. However, if no time is stated, they will be timed out for a default of 5mins.
@@ -33,85 +33,73 @@ module.exports.run = async(client, msg, args) => {
     On the other hand, if the bot's role is one of the highest, the user will be assigned the Timed Out Corner role and it will always be seen from the user list. If they don't have a higher role, the user will be assigned the Time Out Corner role and that's it, you might not be able to see any effects taking place in the user list, which frankly, defeats the purpose of a Time Out Corner.
                 
     With that being said, would you like to change your bot's role position right now? (Just answer yes or no within the next 1 minute.)
-                \`\`\``)
-                msg.channel.awaitMessages(m => m.author.id == hooman.id, {max: 1, time: 60000, errors: ['time']}).then(collected => { 
-                    if (collected.first().content.toLowerCase() == 'yes') {
-                        msg.channel.send(`Alright, come back later!`)
-                    } 
-                    else if (collected.first().content.toLowerCase() == 'no') {
-                        msg.channel.send(`Just remember that if your bot has a lower role, the Time Out role might not be seen in the user role, and that's it!`)
-                        msg.guild.roles.create({
-                            data: {
-                                name: 'Time Out Corner',
-                                color: 'BLACK',
-                                hoist:  true,
-                                position: bothighest
-                            },
-                            reason: 'to time people out',
-                            }).then(result => {
-                                console.log(result.id)
-                                taggedUser.roles.add(result).then(
-                                    (result2) => {
-                                        if (!args[1]) {
-                                            time = 900000
-                                            try {
-                                                usertimeout = timeoutModel.create({
-                                                    userID: hooman.id,
-                                                    serverID: guild,
-                                                    channelID: channel,
-                                                    timeout: time
-                                                })
-                                            } catch (err) {
-                                                console.log(err)
-                                            }
-
-                                            msg.channel.send(`**${taggedUser.displayName}** has been timed out for ${time/60000} minutes (default time). Shame on you!`)
-                                        }
-                                        else {
-                                            time = args[1]
-                                            units = args[1].slice(-1)
-                                            if (units == "s") {
-                                                time = ms(time)
-                                            }
-                                            else if (units == "m") {
-                                                time = ms(time)
-                                            }
-                                            else if (units == "h") {
-                                                time = ms(time)
-                                            }
-                                            else {
-                                                msg.channel.send(`Are you sure you inputted the right format? It should be like this: 12m for 12 minutes. 12s for 12 seconds. 12h for 12 hours.`)
-                                            }
-                                            try {
-                                                usertimeout = timeoutModel.create({
-                                                    userID: hooman.id,
-                                                    serverID: guild,
-                                                    channelID: channel,
-                                                    timeout: time
-                                                }).then(gettimeout => {
-                                                    timeoutData = timeoutModel.findOne({userID: hooman.id, serverID: guild}).then(answers => {
-                                                        if(answers) {
-                                                            msg.channel.send(`**${taggedUser.displayName}** has been timed out for ${time}. Shame on you!`)
-                                                        }
-                                                    }
-                                                    )
-                                                })
-                                            } catch (err) {
-                                                console.log(err)
-                                            }
-                                        }
+                \`\`\``) */
+                msg.channel.send(`If your bot has a lower role, the Time Out role might not be seen in the user role, and that's it!`)
+                msg.guild.roles.create({
+                    data: {
+                        name: 'Time Out Corner',
+                        color: 'BLACK',
+                        hoist:  true,
+                        position: bothighest
+                    },
+                    reason: 'to time people out',
+                    }).then(result => {
+                        console.log(result.id)
+                        taggedUser.roles.add(result).then(
+                            (result2) => {
+                                if (!args[1]) {
+                                    time = 900000
+                                    try {
+                                        usertimeout = timeoutModel.create({
+                                            userID: hooman.id,
+                                            serverID: guild,
+                                            channelID: channel,
+                                            timeout: time
+                                        })
+                                    } catch (err) {
+                                        console.log(err)
                                     }
-                                ).catch((err) => console.error(err))
-                                
-                            })
-                            
-                    }
-                    else {
-                        msg.channel.send(`Right... Okay, goodbye!`)
-                    }
-                }).catch(collected => {
-                    msg.channel.send('No reply after 1 minute, command is cancelled!');
-                });
+
+                                    msg.channel.send(`**${taggedUser.displayName}** has been timed out for ${time/60000} minutes (default time). Shame on you!`)
+                                }
+                                else {
+                                    time = args[1]
+                                    units = args[1].slice(-1)
+                                    if (units == "s") {
+                                        time = ms(time)
+                                    }
+                                    else if (units == "m") {
+                                        time = ms(time)
+                                    }
+                                    else if (units == "h") {
+                                        time = ms(time)
+                                    }
+                                    else {
+                                        msg.channel.send(`Are you sure you inputted the right format? It should be like this: 12m for 12 minutes. 12s for 12 seconds. 12h for 12 hours.`)
+                                    }
+                                    try {
+                                        usertimeout = timeoutModel.create({
+                                            userID: hooman.id,
+                                            serverID: guild,
+                                            channelID: channel,
+                                            timeout: time
+                                        }).then(gettimeout => {
+                                            timeoutData = timeoutModel.findOne({userID: hooman.id, serverID: guild}).then(answers => {
+                                                if(answers) {
+                                                    msg.channel.send(`**${taggedUser.displayName}** has been timed out for ${time}. Shame on you!`)
+                                                }
+                                            }
+                                            )
+                                        })
+                                    } catch (err) {
+                                        console.log(err)
+                                    }
+                                }
+                            }
+                        ).catch((err) => console.error(err))
+                        
+                    })
+                    
             }
             else {
                 taggedUser.roles.add(msg.guild.roles.cache.find(x => x.name == "Time Out Corner"), "");
