5beef153579b8de85614063486650f683e5368aa
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-11 05:29:03 +0800

    changes

--- commands/money/wallet.js
+++ commands/money/wallet.js
@@ -3,52 +3,45 @@ const Discord = require('discord.js');
 module.exports.run = async(client, msg, args) => {
     let hooman = msg.author.id
     let server = msg.guild.id
-    msg.channel.send(`bro i dont even know`)
-// moneyModel.create({
-// userID: hooman,
-// serverID: server
-// })
-// let hooman = msg.author.id
-// let server = msg.guild.id
-// moneyModel.findOne({userID: hooman}).then(moneyData => {
-// console.log(moneyData)
-// if (!moneyData) {
-// msg.channel.send(`Oh, you do not have a wallet! Let me create one for you...`)
-// money = 0
-// let wallet;
-// try {
-// wallet = moneyModel.create({
-// userID: hooman,
-// serverID: server
-// }).then(result => {
-// await moneyModel.findOne({userID: hooman}).then(newmoneyData => {
-// url = `https://cdn.discordapp.com/avatars/${hooman}/${msg.author.avatar}.png`
-// const embed = new Discord.MessageEmbed()
-// .setColor('#FF69B4')
-// .setTitle(`**${msg.guild.members.cache.get(newmoneyData.userID).displayName}'s** Wallet`)
-// .setDescription('You can see your money here.')
-// .addFields(
-// { name: `Name: ${msg.guild.members.cache.get(newmoneyData.userID).displayName}`, value: `Gold: ${newmoneyData.gold} gp`}
-// )
-// .setThumbnail(`${url}`);
-// msg.channel.send(embed);
-// })
-// })
-// } catch (err) {
-// console.log(err)
-// }
-// }
-// else {
-// url = `https://cdn.discordapp.com/avatars/${hooman}/${msg.author.avatar}.png`
-// const embed = new Discord.MessageEmbed()
-// .setColor('#FF69B4')
-// .setTitle(`**${msg.guild.members.cache.get(moneyData.userID).displayName}'s** Wallet`)
-// .setDescription('You can see your money here.')
-// .addFields(
-// { name: `Name: ${msg.guild.members.cache.get(moneyData.userID).displayName}`, value: `Gold: ${moneyData.gold} gp`}
-// )
-// .setThumbnail(`${url}`);
-// msg.channel.send(embed);
-// }
-// })
+    moneyModel.findOne({userID: hooman}).then(moneyData => {
+    console.log(moneyData)
+    if (!moneyData) {
+    msg.channel.send(`Oh, you do not have a wallet! Let me create one for you...`)
+    money = 0
+    let wallet;
+    try {
+    wallet = moneyModel.create({
+    userID: hooman,
+    serverID: server
+    }).then(result => {
+    await moneyModel.findOne({userID: hooman}).then(newmoneyData => {
+    url = `https://cdn.discordapp.com/avatars/${hooman}/${msg.author.avatar}.png`
+    const embed = new Discord.MessageEmbed()
+    .setColor('#FF69B4')
+    .setTitle(`**${msg.guild.members.cache.get(newmoneyData.userID).displayName}'s** Wallet`)
+    .setDescription('You can see your money here.')
+    .addFields(
+    { name: `Name: ${msg.guild.members.cache.get(newmoneyData.userID).displayName}`, value: `Gold: ${newmoneyData.gold} gp`}
+    )
+    .setThumbnail(`${url}`);
+    msg.channel.send(embed);
+    })
+    })
+    } catch (err) {
+    console.log(err)
+    }
+    }
+    else {
+    url = `https://cdn.discordapp.com/avatars/${hooman}/${msg.author.avatar}.png`
+    const embed = new Discord.MessageEmbed()
+    .setColor('#FF69B4')
+    .setTitle(`**${msg.guild.members.cache.get(moneyData.userID).displayName}'s** Wallet`)
+    .setDescription('You can see your money here.')
+    .addFields(
+    { name: `Name: ${msg.guild.members.cache.get(moneyData.userID).displayName}`, value: `Gold: ${moneyData.gold} gp`}
+    )
+    .setThumbnail(`${url}`);
+    msg.channel.send(embed);
+    }
+    })
 }
