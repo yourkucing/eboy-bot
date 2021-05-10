15fbd246410e454c211d9b5255dc348c7ee98679
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-11 04:53:46 +0800

    Update wallet.js

--- commands/money/money/wallet.js
+++ commands/money/money/wallet.js
@@ -4,44 +4,48 @@ const Discord = require('discord.js');
 module.exports.run = async(client, msg, args) => {
     let hooman = msg.author.id
     let server = msg.guild.id
-    moneyData = await moneyModel.findOne({userID: hooman})
-    console.log(moneyData)
-    if (!moneyData) {
-        msg.channel.send(`Oh, you do not have a wallet! Let me create one for you...`)
-        money = 0
-
-        let wallet;
-        try {
-            wallet = moneyModel.create({
-                userID: hooman,
-                serverID: server
-            }).then(result => {
-                newmoneyData = await moneyModel.findOne({userID: hooman})
-                url = `https://cdn.discordapp.com/avatars/${hooman}/${msg.author.avatar}.png`
-                const embed = new Discord.MessageEmbed()
-                .setColor('#FF69B4')
-                .setTitle(`**${msg.guild.members.cache.get(newmoneyData.userID).displayName}'s** Wallet`)
-                .setDescription('You can see your money here.')
-                .addFields(
-                { name: `Name: ${msg.guild.members.cache.get(newmoneyData.userID).displayName}`, value: `Gold: ${newmoneyData.gold} gp`}
-                )
-                .setThumbnail(`${url}`);
-                msg.channel.send(embed);
-            })
-        } catch (err) {
-            console.log(err)
+    moneyModel.findOne({userID: hooman}).then(moneyData => {
+        console.log(moneyData)
+        if (!moneyData) {
+            msg.channel.send(`Oh, you do not have a wallet! Let me create one for you...`)
+            money = 0
+    
+            let wallet;
+            try {
+                wallet = moneyModel.create({
+                    userID: hooman,
+                    serverID: server
+                }).then(result => {
+                    await moneyModel.findOne({userID: hooman}).then(newmoneyData => {
+                        url = `https://cdn.discordapp.com/avatars/${hooman}/${msg.author.avatar}.png`
+                        const embed = new Discord.MessageEmbed()
+                        .setColor('#FF69B4')
+                        .setTitle(`**${msg.guild.members.cache.get(newmoneyData.userID).displayName}'s** Wallet`)
+                        .setDescription('You can see your money here.')
+                        .addFields(
+                        { name: `Name: ${msg.guild.members.cache.get(newmoneyData.userID).displayName}`, value: `Gold: ${newmoneyData.gold} gp`}
+                        )
+                        .setThumbnail(`${url}`);
+                        msg.channel.send(embed);
+                    })
+                    
+                })
+            } catch (err) {
+                console.log(err)
+            }
         }
-    }
-    else {
-        url = `https://cdn.discordapp.com/avatars/${hooman}/${msg.author.avatar}.png`
-        const embed = new Discord.MessageEmbed()
-        .setColor('#FF69B4')
-        .setTitle(`**${msg.guild.members.cache.get(moneyData.userID).displayName}'s** Wallet`)
-        .setDescription('You can see your money here.')
-        .addFields(
-        { name: `Name: ${msg.guild.members.cache.get(moneyData.userID).displayName}`, value: `Gold: ${moneyData.gold} gp`}
-        )
-        .setThumbnail(`${url}`);
-        msg.channel.send(embed);
-    }
+        else {
+            url = `https://cdn.discordapp.com/avatars/${hooman}/${msg.author.avatar}.png`
+            const embed = new Discord.MessageEmbed()
+            .setColor('#FF69B4')
+            .setTitle(`**${msg.guild.members.cache.get(moneyData.userID).displayName}'s** Wallet`)
+            .setDescription('You can see your money here.')
+            .addFields(
+            { name: `Name: ${msg.guild.members.cache.get(moneyData.userID).displayName}`, value: `Gold: ${moneyData.gold} gp`}
+            )
+            .setThumbnail(`${url}`);
+            msg.channel.send(embed);
+        }
+    })
+
 }
