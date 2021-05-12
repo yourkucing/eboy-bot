9c887f0ff8ce9e31e96dc179bb5fe10a7ffaec4c
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-13 00:25:09 +0800

    Update getout.js

--- commands/admin/getout.js
+++ commands/admin/getout.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const timeoutModel = require('./models/timeoutSchema');
 
 module.exports.run = async(client, msg, args) => {
     msg.channel.send('hi')
@@ -30,42 +29,6 @@ module.exports.run = async(client, msg, args) => {
                 console.log(`Something went wrong`)
             }
         })
-        // if (!msg.mentions.users.size) {
-        //     msg.channel.send(`Uh... Do I remove the role of the server's ghost? o.o`)
-        //     return
-        // }
-        // else {
-        //     const taggedUser = msg.mentions.members.first();
-        //     serverID = args[0]
-        //     timeoutModel.findOne({userID: taggedUser.id, serverID: serverID}).then(timeout => {
-        //         if (timeout) {
-        //             const timeoutrole = msg.guild.roles.cache.find(role => role.name === "Time Out Corner")
-        //             const channel = client.channels.cache.get(channelID)
-        //             taggedUser.roles.remove(timeoutrole.id).catch((e) => {console.log(e)})
-        //             channel.send(`<@${taggedUser.id}>, your timeout has ended!`)
-        //             timeoutModel.deleteOne({userID: taggedUser.id, serverID: serverID}).then(r => {
-        //                 if (r) {
-        //                     console.log(`Success deletion!`)
-        //                 }
-        //                 else {
-        //                     console.log(`Error in deletion from timeout db.`)
-        //                 }
-        //             })
-        //         }
-        //         else {
-        //             timeoutrole = msg.guild.roles.cache.find(role => role.name === "Time Out Corner")
-        //             if (taggedUser.roles.cache.some((role) => role.id === timeoutrole.id)) {
-        //                 taggedUser.roles.remove(timeoutrole.id).catch((e) => {console.log(e)})
-        //                 channel.send(`<@${taggedUser.id}>, your timeout has ended!`)
-        //             }
-        //             else {
-        //                 msg.channel.send(`They're not in timeout!`)
-        //             }
-        //         }
-        //     })
-            
-
-        // }
     }
     else {
         msg.channel.send("You do not have admin rights to use this.")
