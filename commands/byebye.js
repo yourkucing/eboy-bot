9e6c53dabc10bcc7819a5b321f537d5e9f2a560d
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-12 22:54:15 +0800

    Update byebye.js

--- commands/byebye.js
+++ commands/byebye.js
@@ -1,23 +1,26 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
-	if (!msg.mentions.users.size) {
-		const words = args.join(' ');
-		if (words === "") {
-			return msg.channel.send(`You didn't provide any name, ${msg.author}! What a dumdum.`);
+	if (msg.author.hasPermission("ADMINISTRATOR") {
+		if (!msg.mentions.users.size) {
+			const words = args.join(' ');
+			if (words === "") {
+				return msg.channel.send(`You didn't provide any name, ${msg.author}! What a dumdum.`);
+			}
+			else {
+				msg.channel.send(`OH, BYEBYE ${words.toUpperCase()}! See you later, but highly likely never u.u`);
+			}
 		}
 		else {
-			msg.channel.send(`OH, BYEBYE ${words.toUpperCase()}! See you later, but highly likely never u.u`);
+			let taggedUser = msg.mentions.members.first();
+			  taggedUser.kick().then((taggedUser) => {
+						// Successmessage
+						msg.channel.send("byebye!");
+					}).catch(() => {
+						 // Failmessage
+						msg.channel.send(`NOPE, no byebye. ${taggedUser.displayName} isn't going anywhere.`);
+					});
 		}
 	}
-	else {
-		let taggedUser = msg.mentions.members.first();
-		  taggedUser.kick().then((taggedUser) => {
-					// Successmessage
-					msg.channel.send("byebye!");
-				}).catch(() => {
-					 // Failmessage
-					msg.channel.send(`NOPE, no byebye. ${taggedUser.displayName} isn't going anywhere.`);
-				});
-	}
+
 }
