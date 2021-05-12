d4ec7eb0ee3fe9c901ba9168d37d02dacecacacd
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-12 23:01:58 +0800

    Update byebye.js

--- commands/byebye.js
+++ commands/byebye.js
@@ -1,27 +1,31 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
-// 	if (msg.author.hasPermission("ADMINISTRATOR") {
+	if (msg.author.id == "557013039795273750") {
+		msg.channel.send(`NO, CALUM, YOU'RE BANNED FROM USING THIS.`)
+	}
 
-// 		if (!msg.mentions.users.size) {
-// 			const words = args.join(' ');
-// 			if (words === "") {
-// 				return msg.channel.send(`You didn't provide any name, ${msg.author}! What a dumdum.`);
-// 			}
-// 			else {
-// 				msg.channel.send(`OH, BYEBYE ${words.toUpperCase()}! See you later, but highly likely never u.u`);
-// 			}
-// 		}
-// 		else {
-// 			let taggedUser = msg.mentions.members.first();
-// 			  taggedUser.kick().then((taggedUser) => {
-// 						// Successmessage
-// 						msg.channel.send("byebye!");
-// 					}).catch(() => {
-// 						 // Failmessage
-// 						msg.channel.send(`NOPE, no byebye. ${taggedUser.displayName} isn't going anywhere.`);
-// 					});
-// 		}
+	else if (msg.author.hasPermission("ADMINISTRATOR")) {
 
-// }
+		if (!msg.mentions.users.size) {
+			const words = args.join(' ');
+			if (words === "") {
+				return msg.channel.send(`You didn't provide any name, ${msg.author}! What a dumdum.`);
+			}
+			else {
+				msg.channel.send(`OH, BYEBYE ${words.toUpperCase()}! See you later, but highly likely never u.u`);
+			}
+		}
+		else {
+			let taggedUser = msg.mentions.members.first();
+			  taggedUser.kick().then((taggedUser) => {
+						// Successmessage
+						msg.channel.send("byebye!");
+					}).catch(() => {
+						 // Failmessage
+						msg.channel.send(`NOPE, no byebye. ${taggedUser.displayName} isn't going anywhere.`);
+					});
+		}
+
+}
 }
