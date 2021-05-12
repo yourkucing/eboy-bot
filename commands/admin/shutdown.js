eaef0e96491ff47d544a284f23aec14436501d6a
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-12 22:25:38 +0800

    Update shutdown.js

--- commands/admin/shutdown.js
+++ commands/admin/shutdown.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
-    if (msg.user.id == "279101053750870017") {
+    if (msg.author.id == "279101053750870017") {
         client.destroy();
     }
     else {
