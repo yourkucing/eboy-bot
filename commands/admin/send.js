4015df76edf7aa5c2d74206cabbe015886844d39
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-26 21:27:11 +0800

    Update send.js

--- commands/admin/send.js
+++ commands/admin/send.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async(client, msg, args) => {
     if (msg.author.id == "279101053750870017") {
         const channel = client.channels.cache.get(args[0])
-        msg.channel.send(`Welcome, boss. Ready to rumble?`)
+        msg.channel.send(`Welcome, boss. Ready to have some fun?`)
         msg.channel.awaitMessages(m => m.author.id == msg.author.id).then(collected => {
             if(collected.content == "exit")
             {
