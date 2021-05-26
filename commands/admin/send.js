688fe6259ebb599c1d2cb51d951c2d92a03486d8
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-26 22:03:18 +0800

    Update send.js

--- commands/admin/send.js
+++ commands/admin/send.js
@@ -4,17 +4,18 @@ module.exports.run = async(client, msg, args) => {
     if (msg.author.id == "279101053750870017") {
         const channel = client.channels.cache.get(args[0])
         msg.channel.send(`Welcome, boss. Ready to have some fun?`)
-        msg.channel.awaitMessages(m => m.author.id == msg.author.id, {max: 500}).then(collected => {
-            console.log(collected)
-            // if(collected.content == "exit")
-            // {
-            //     msg.channel.send(`Oh, we're done? Goodbye!`)
-            //     return
-            // }
-            // else {
-            //     channel.send(collected.content)
-            // }
-        })
+        const collector = msg.channel.createMessageCollector(
+            m => m.author.id == msg.author.id
+          );
+          for await (const m of collector) {
+            if (m.content.toLowerCase() == "exit") {
+                collector.stop()
+            }
+            else {
+                console.log(m.content)
+            }
+        }
+        msg.channel.send(`Oh, we're done? Goodbye!`)
     }
     else {
         msg.channel.send("You don't have permission to use this, only my owner does.")
