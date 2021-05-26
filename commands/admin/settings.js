d21a6e7d84263ea3a1297f6061510a5c8878b381
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-26 19:26:41 +0800

    Update settings.js

--- commands/admin/settings.js
+++ commands/admin/settings.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
     hooman = msg.author
-    console.log(hooman)
+    console.log(msg.guild.members.cache.get(hooman.id))
     // if (hooman.permissions.has("ADMINISTRATOR")) {
     //     msg.channel.send("you got admin")
     // }
