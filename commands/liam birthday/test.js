f61c1e4ec9c4c4c1300e5009f19d42433d51eb97
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-27 20:32:34 +0800

    Update test.js

--- commands/liam birthday/test.js
+++ commands/liam birthday/test.js
@@ -1,12 +1,16 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
-    msg.channel.awaitMessages(m => m.author.id == "279101053750870017" && m.content == "CODE", {max: 10}).then(collected => {
-        msg.channel.send(`\`Liam keyed in the code and successfully unlocked the door. But... it was empty? Where did the meowing come from then? He saw another piece of paper. This time, all it said was:\``)
-        msg.channel.send(`*You have come to the end of the adventure! But wait, where's Amy? If you want to know, just type* \`uwu amy\``)
-        return
-    }).catch(collected => {
-        msg.channel.send(`You did not get it! That's 10 tries. Unfortunately, you have to restart: \`uwu adventure\``)
-        return
+    const collector = msg.channel.createMessageCollector(
+        m => m.author.id == "279101053750870017", {max: 10}
+      );
+    collector.on('collect', m => {
+        if(m.content == "CODE") {
+            msg.channel.send(`YOU GOT IT!`)
+            collector.stop()
+        }
+    });
+    collector.on('end', collected => {
+        msg.channel.send(`Time's up too bad!`)
     });
 }
