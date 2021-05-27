242e72bab27d1a5b65d2f48967d8699517c18380
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-27 20:11:19 +0800

    Update adventure.js

--- commands/liam birthday/adventure.js
+++ commands/liam birthday/adventure.js
@@ -27,10 +27,10 @@ module.exports.run = async(client, msg, args) => {
                     msg.channel.send(`*Hi Liam,\nAre you looking for Amy? Don't worry, she's fine actually. I mean, other than the fact that she keeps meowing for you. You must be an amazing pet owner, I don't doubt that. But anyway, I just wanted to take this time to tell you that you are:\n\nClever: YOU ARE. I mean, the way your mind works, I'm always impressed.\nOrganised: Have you seen yourself when taking charge of the Limes? You're one of my role models.\nDorky: The way you spazz about your own artwork is adorable as hell.\nEasy-going: I love being able to talk to you sometimes, it makes me feel comfortable and you're always fun to talk to.
                     
 Anyway, that's pretty much it. I should be nicer, I guess. Since it's your birthday and all. There is a small room behind the mirror, but there's a code. You gotta put on your thinking CAP. But I have faith in you.*`)
-                    msg.channel.send(`\`Liam frowned and walked towards the mirror. He swore he could hear a short meow from behind the mirror. Pushing it aside, sure enough, there was a door. But there's a code too. Now what could it be?
+                    msg.channel.send(`\`Liam frowned and walked towards the mirror. He swore he could hear a short meow from behind the mirror. Pushing it aside, sure enough, there was a door. But there's a code too. Now what could it be?\`
                     
-You have 10 tries to figure out the code. Hint: put them in capital letters, and you already know the code the moment you stepped into this room. Good luck! (Just reply with the code, 10 tries!)\``)
-                    msg.channel.awaitMessages(m => m.author.id == "279101053750870017" && m.content === "CODE", {max: 10}).then(collected => {
+\`You have 10 tries to figure out the code. Hint: put them in capital letters, and you already know the code the moment you stepped into this room. Good luck! (Just reply with the code, 10 tries!)\``)
+                    msg.channel.awaitMessages(m => m.author.id == "279101053750870017" && m.content == "CODE", {max: 10}).then(collected => {
                         msg.channel.send(`\`Liam keyed in the code and successfully unlocked the door. But... it was empty? Where did the meowing come from then? He saw another piece of paper. This time, all it said was:\``)
                         msg.channel.send(`*You have come to the end of the adventure! But wait, where's Amy? If you want to know, just type* \`uwu amy\``)
                         return
