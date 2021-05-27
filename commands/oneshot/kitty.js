52656078cc110fd33dbe1211cc5cac2166127374
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-28 00:44:26 +0800

    changes

---
+++ commands/oneshot/kitty.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');
+
+module.exports.run = async(client, msg, args) => {
+    msg.channel.send(`\`He frowned and suddenly felt something moving in his backpack. He quickly realised that there was a small opening, and OH! A small black nose popped out. AMY! She was in his bag all along.\`\n\nHEHEEHE. I hope you enjoyed that tiny adventure and it made you happy even for a little bit! Go on, enjoy the rest of your day!`)
+
+}
