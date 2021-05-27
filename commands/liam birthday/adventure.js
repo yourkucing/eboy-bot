e0267db00c398b1852077ae6e52bea56e99d4a4b
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-27 21:49:17 +0800

    Update adventure.js

--- commands/liam birthday/adventure.js
+++ commands/liam birthday/adventure.js
@@ -10,7 +10,7 @@ module.exports.run = async(client, msg, args) => {
 \n\n:arrow_right: \`Go into the room beside the fireplace.\`
 :arrow_up: \`Go up the marble staircase\`
         
-\`Make your choice, Liam. Choose fast, you only have 1 min!\``).then(msg => {
+\`Make your choice, Liam. Choose fast, you only have 10 mins!\``).then(msg => {
             msg.react('➡️').then(message => {
                 msg.react('⬆️')
                 const rightFilter = (reaction, user) => {
@@ -19,8 +19,8 @@ module.exports.run = async(client, msg, args) => {
                 const upFilter = (reaction, user) => {
                     return reaction.emoji.name === '⬆️' && !user.bot;
                 };
-                const right = msg.createReactionCollector(rightFilter, { time: 60000 });
-                const up = msg.createReactionCollector(upFilter, { time: 60000 });
+                const right = msg.createReactionCollector(rightFilter, { time: 600000 });
+                const up = msg.createReactionCollector(upFilter, { time: 600000 });
 
                 right.on('collect', (reaction, user) => {
                     msg.channel.send(`\`Liam walked towards the door, trying to turn its knob. Surprisingly, it's unlocked! With quiet footsteps, he wandered into the room— and stopped cold in his tracks. What's this? A letter addressed to him? He picked it up and read its contents.\``)
@@ -30,14 +30,54 @@ Anyway, that's pretty much it. I should be nicer, I guess. Since it's your birth
                     msg.channel.send(`\`Liam frowned and walked towards the mirror. He swore he could hear a short meow from behind the mirror. Pushing it aside, sure enough, there was a door. But there's a code too. Now what could it be?\`
                     
 \`You have 10 tries to figure out the code. Hint: put them in capital letters, and you already know the code the moment you stepped into this room. Good luck! (Just reply with the code, 10 tries!)\``)
-                    msg.channel.awaitMessages(m => m.author.id == "279101053750870017" && m.content == "CODE", {max: 10}).then(collected => {
-                        msg.channel.send(`\`Liam keyed in the code and successfully unlocked the door. But... it was empty? Where did the meowing come from then? He saw another piece of paper. This time, all it said was:\``)
-                        msg.channel.send(`*You have come to the end of the adventure! But wait, where's Amy? If you want to know, just type* \`uwu amy\``)
-                        return
-                    }).catch(collected => {
-                        msg.channel.send(`You did not get it! That's 10 tries. Unfortunately, you have to restart: \`uwu adventure\``)
-                        return
+
+                    const collector = msg.channel.createMessageCollector(
+                        m => m.author.id == "279101053750870017", {max: 10}
+                    );
+                    collector.on('collect', m => {
+                        if (m.content == "CODE") {
+                            msg.channel.send(`\`Liam keyed in the code and successfully unlocked the door. But... it was empty? Where did the meowing come from then? He saw another piece of paper. This time, all it said was:\``)
+                            msg.channel.send(`*You have come to the end of the adventure! But wait, where's Amy? If you want to know, just type* \`uwu amy\``)
+                            collector.stop("CODE")
+                        }
                     });
+                    collector.on('end', collected => {
+                        if (collected.size == 10) {
+                            msg.channel.send(`You did not get it! That's 10 tries. Unfortunately, you have to restart: \`uwu adventure\``)
+                            return
+                        }
+                    });
+
+                    right.stop()
+                    up.stop()
+                    msg.reactions.removeAll()
+                })
+
+                left.on('collect', (reaction, user) => {
+                    msg.channel.send(`\`Liam decided to walk up the stairs. After all, Amy might be upstairs. Straight up was a huge foyer and of course, in the middle of that foyer was a note. He picked it up, curious. The note said:\``)
+                    msg.channel.send(`*Liam, hihihi!\nMan, I'm really sorry I took your cat. It was a mistake though, I just wanted to play catch with her but she ended up lost. I tracked her here. She's probably somewhere but I'll tell you later. First things first though, I just wanted to tell you that you're a pretty amazing human being. I mean, if I'm being honest, every time you come into the server being your cool self, the happiness _doubled_ and it's true! I hope you realise how important you are to us, especially most of the EC gang. So please, never forget that okay?\n\nAnyway, just walk straight ahead and you'll see another door. But it has a lock! Good luck!*`)
+                    msg.channel.send(`\`Liam frowned and walked towards the door at the end of the hall. Sure enough, he heard some sort of meowing. That must be Amy! But... the door is locked and there's a padlock with 4 digits. The first digit is already shown. It's 2. All he needed are three more digits.\`\n\n\`Key in the answer in this format: digit2,digit3,digit4 (eg. 2,3,4). Take note that there are no spaces in between or at the sides. Hint: You already know the answer from this hall. You have ten tries. Good luck!\``)
+
+                    const collector = msg.channel.createMessageCollector(
+                        m => m.author.id == "279101053750870017", {max: 10}
+                    );
+                    collector.on('collect', m => {
+                        if (m.content == "4,6,8") {
+                            msg.channel.send(`\`Liam keyed in the code and successfully unlocked the door. But... it was empty? Where did the meowing come from then? He saw another piece of paper. This time, all it said was:\``)
+                            msg.channel.send(`*You have come to the end of the adventure! But wait, where's Amy? If you want to know, just type* \`uwu amy\``)
+                            collector.stop("4,6,8")
+                        }
+                    });
+                    collector.on('end', collected => {
+                        if (collected.size == 10) {
+                            msg.channel.send(`You did not get it! That's 10 tries. Unfortunately, you have to restart: \`uwu adventure\``)
+                            return
+                        }
+                    });
+
+                    right.stop()
+                    up.stop()
+                    msg.reactions.removeAll()
                 })
 
             })
