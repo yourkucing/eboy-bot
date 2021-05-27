97632f7429f5a781ee2dc6885b534f3838608832
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-27 22:00:56 +0800

    changes

---
+++ commands/liam birthday/amy.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js');
+
+module.exports.run = async(client, msg, args) => {
+    if (msg.channel.id == "847316136433025045" && msg.author.id == "279101053750870017") {
+        msg.channel.send(`\`Liam frowned and suddenly felt something moving in his backpack. He quickly realised that there was a small opening, and OH! A small black nose popped out. AMY! She was in his bag all along.\`\n\nHEHEEHE. I hope you enjoyed that tiny adventure and it made you happy even for a little bit! Go on, enjoy the rest of your day!`)
+    }
+}
