1c04738d162e0cabeebd5266b936b8fc4c012384
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-05-27 20:36:10 +0800

    Update test.js

--- commands/liam birthday/test.js
+++ commands/liam birthday/test.js
@@ -1,16 +1,20 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
+    n = "no"
     const collector = msg.channel.createMessageCollector(
         m => m.author.id == "279101053750870017", {max: 10}
       );
     collector.on('collect', m => {
         if(m.content == "CODE") {
             msg.channel.send(`YOU GOT IT!`)
+            n = "yes"
             collector.stop()
         }
     });
     collector.on('end', collected => {
-        msg.channel.send(`Time's up too bad!`)
+        if (n = "no"){
+            msg.channel.send(`Time's up too bad!`)
+        }
     });
 }
