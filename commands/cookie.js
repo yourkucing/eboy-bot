026ff036954d2107af29f8b3262a30b6133461ed
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-06-14 22:07:46 +0800

    changes

--- commands/cookie.js
+++ commands/cookie.js
@@ -10,93 +10,38 @@ module.exports.run = async(client, msg, args) => {
         hooman = msg.author
         taggedUser = msg.mentions.members.first();
 
-        cookieData = await cookieModel.findOne({userID: hooman.id})
 		taggedData = await cookieModel.findOne({userID: taggedUser.id})
 
-        if (cookieData && taggedData) {
-            await cookieModel.findOneAndUpdate({userID: hooman.id}, {
-                $set: {
-                    cookie: cookieData.cookie - 1
-                }
-            })
-
+        if (taggedData) {
             await cookieModel.findOneAndUpdate({userID: taggedUser.id}, {
                 $set: {
                     cookie: taggedData.cookie + 1
                 }
             })
+            const embed = new Discord.MessageEmbed()
+            .setColor('#FF69B4')
+            .setDescription(`${msg.guild.members.cache.get(hooman.id).displayName} gave **${taggedUser.displayName}** a cookie yay! :cookie:`)
+            .setFooter(`${taggedUser.displayName} has ${taggedData.cookie} cookies.`);
+            msg.channel.send(embed);
         }
-        else if (!cookieData) {
+        else {
             cookie = cookieModel.create({
-                userID: hooman.id
-            }).catch((e) => { console.log(e); }).then(r => {
-                if (!r) {
+                userID: taggedUser.id
+            }).catch((e) => { console.log(e); }).then(t => {
+                if (!t) {
                     msg.channel.send(`\`Something went wrong. Please try again or contact Maryam#9206 if error persists.\``)
                     console.log(r)
                     return
                 }
                 else {
-                    if (!taggedData) {
-                        cookie2 = cookieModel.create({
-                            userID: taggedUser.id
-                        }).catch((e) => { console.log(e); }).then(t => {
-                            if (!t) {
-                                msg.channel.send(`\`Something went wrong. Please try again or contact Maryam#9206 if error persists.\``)
-                                console.log(r)
-                                return
-                            }
-                            else {
-                                cookieModel.findOneAndUpdate({userID: taggedUser.id}, {
-                                    $set: {
-                                        cookie: 1
-                                    }
-                                }).then(y => {
-                                    console.log('Successful!')
-                                })
-                            }
-                        })
-                    }
-                    else {
-                        cookieModel.findOneAndUpdate({userID: taggedUser.id}, {
-                            $set: {
-                                cookie: taggedData.cookie + 1
-                            }
-                        }).then(z =>
-                            {
-                                console.log('Successful!')
-                            })
-                    }
+                    console.log('Successful!')
                 }
             })
-        }
-        else {
-            if (!taggedData) {
-                cookie2 = cookieModel.create({
-                    userID: taggedUser.id
-                }).catch((e) => { console.log(e); }).then(t => {
-                    if (!t) {
-                        msg.channel.send(`\`Something went wrong. Please try again or contact Maryam#9206 if error persists.\``)
-                        console.log(r)
-                        return
-                    }
-                    else {
-                        cookieModel.findOneAndUpdate({userID: hooman.id}, {
-                            $set: {
-                                cookie: cookieData.cookie - 1
-                            }
-                        }).then(y => {
-                            cookieModel.findOneAndUpdate({userID: taggedUser.id}, {
-                                $set: {
-                                    cookie: 1
-                                }
-                            }).then(z =>
-                                {
-                                    console.log('Successful!')
-                                })
-                        })
-                    }
-                })
-            }
+            const embed = new Discord.MessageEmbed()
+            .setColor('#FF69B4')
+            .setDescription(`${msg.guild.members.cache.get(hooman.id).displayName} gave **${taggedUser.displayName}** a cookie yay! :cookie:`)
+            .setFooter(`${taggedUser.displayName} has 1 cookie.`);
+            msg.channel.send(embed);
         }
     }
 }
--- models/cookieSchema.js
+++ models/cookieSchema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const cookieSchema = new mongoose.Schema({
     userID: {type: String, require: true, unique: true},
-    cookie: {type: Number, default: 0}
+    cookie: {type: Number, default: 1}
 })
 
 const model = mongoose.model('cookieModels', cookieSchema);
