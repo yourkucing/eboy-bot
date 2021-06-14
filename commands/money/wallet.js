a626e37493334356196b4f404a9bb7f18daa2f6f
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-06-14 22:29:38 +0800

    Update wallet.js

--- commands/money/wallet.js
+++ commands/money/wallet.js
@@ -1,4 +1,5 @@
 const moneyModel = require('../../models/moneySchema');
+const cookieModel = require('../../models/cookieSchema')
 const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
@@ -7,9 +8,16 @@ module.exports.run = async(client, msg, args) => {
     server = msg.guild.id
 
     moneyData = await moneyModel.findOne({userID: hooman})
+    cookieData = await cookieModel.findOne({userID: hooman})
     console.log(moneyData)
 
     if (!moneyData) {
+        if (!cookieData) {
+            cookie = 0
+        }
+        else {
+            cookie = cookieData.cookie
+        }
         wallet = moneyModel.create({
             userID: hooman,
             serverID: server,
@@ -24,7 +32,7 @@ module.exports.run = async(client, msg, args) => {
                     .setTitle(`**${msg.guild.members.cache.get(moneyData.userID).displayName}'s** Wallet`)
                     .setDescription('You can see your money here.')
                     .addFields(
-                    { name: `Name: ${msg.guild.members.cache.get(moneyData.userID).displayName}`, value: `Gold: ${moneyData.gold} gp`}
+                    { name: `Name: ${msg.guild.members.cache.get(moneyData.userID).displayName}`, value: `Gold: ${moneyData.gold} gp\nCookie: ${cookie}`}
                     )
                     .setThumbnail(`${url}`);
                     msg.channel.send(embed);
@@ -39,13 +47,19 @@ module.exports.run = async(client, msg, args) => {
         
     }
     else {
+        if (!cookieData) {
+            cookie = 0
+        }
+        else {
+            cookie = cookieData.cookie
+        }
         url = `https://cdn.discordapp.com/avatars/${hooman}/${msg.author.avatar}.png`
         const embed = new Discord.MessageEmbed()
         .setColor('#FF69B4')
         .setTitle(`**${msg.guild.members.cache.get(moneyData.userID).displayName}'s** Wallet`)
         .setDescription('You can see your money here.')
         .addFields(
-        { name: `Name: ${msg.guild.members.cache.get(moneyData.userID).displayName}`, value: `Gold: ${moneyData.gold} gp`}
+        { name: `Name: ${msg.guild.members.cache.get(moneyData.userID).displayName}`, value: `Gold: ${moneyData.gold} gp\nCookie: ${cookie}`}
         )
         .setThumbnail(`${url}`);
         msg.channel.send(embed);
