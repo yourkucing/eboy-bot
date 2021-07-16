0cf312f577eb5673eb33ccd2cb6631d1ae64245b
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-16 17:21:21 +0800

    Create convert.js

---
+++ commands/decisions/convert.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+module.exports.run = async(client, msg, args) => {
+    if (!args || args.length == 0) {
+        msg.channel.send(`What do you want to convert, mate? Please retry.`)
+        return
+    }
+    else {
+        d = new Date()
+        msg.channel.send(`Now: ${d}`)
+        utc_offset = d.getTimezoneOffset()
+        d.setMinutes(d.getMinutes() + utc_offset)
+        msg.channel.send(`UTC: ${d}`)
+        cest_offset = 2 * 60
+        d.setMinutes(d.getMinutes() + cest_offset)
+        msg.channel.send(`Denmark: ${d}`)
+    }
+}
