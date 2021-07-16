a636abb1c0a02a4ca565be002ea655d2d62cf9bb
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-16 14:15:42 +0800

    Update birthdaySchema.js

--- models/birthdaySchema.js
+++ models/birthdaySchema.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const birthdaySchema = new mongoose.Schema({
-    serverID: {type: String, require: true, unique: true},
+    serverID: {type: String, require: true},
     userID: {type: String, require: true},
+    channelID: {type: String, require: true},
     birthday: {type: Date, require: true}
 })
 
