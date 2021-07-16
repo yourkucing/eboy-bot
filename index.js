94e6485871cf350cd5d0a87f0a1af348cda54649
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-16 16:50:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const checkforTimeouts = async() => {
 const checkforBirthdays = async() => {
 	day = new Date().getDate()
 	month = new Date().getMonth() + 1
-	if (new Date().getUTCHours() == 8 && new Date().getUTCMinutes() == 25) {
+	if (new Date().getUTCHours() == 0 && new Date().getUTCMinutes() == 0) {
 		birthday = new Date(`2000-${month}-${day}`)
 		const query = {
 			birthday: birthday
@@ -156,6 +156,12 @@ client.on('ready', () => {
 		console.log(err)
 	})
  });
+
+ client.on('guildCreate', guild => {
+
+	guild.systemChannel.send(`Hello there! Thank you for using Eboy bot uwu!\n\nYou can start off by doing \`uwu help\` to see all the relevant commands. You can also enable interactions by eboy (mainly having him reply to your goodmorning or goodnight) by running the command \`uwu enable-interactions\`. Of course, only admins can do this!\n\nMaryam#9206 built me. If you have any questions, complaints or suggestions, you can DM her.\nYou can also submit your suggestion or feedback here: https://forms.gle/GpoHX32kVQznvY3h6 though dont forget to leave down your name and a way to contact you, just in case there are questions we need to specify! (∩•̀ω•́)⊃-⋆`)
+	
+	});	
 
 client.on('message', msg => {
 
