ca01b1e1bf81b754e1cc51b9af9db8c1ab9d0bb1
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-16 13:04:04 +0800

    Update birthday.js

--- commands/fun/birthday.js
+++ commands/fun/birthday.js
@@ -5,7 +5,7 @@ module.exports.run = async(client, msg, args) => {
     hooman = msg.author.id
     server = msg.guild.id
     months = {january: 01, february: 02, march: 03, april: 04, may: 05, june: 06, july: 07, august: 08, september: 09, october: 10, november: 11, december: 12, jan: 01, feb: 02, mar: 03, apr: 04, jun: 06, jul: 07, aug: 08, sep: 09, oct: 10, nov: 11, dec: 12}
-    if (!args) {
+    if (!args || args.length == 0) {
         birthdaykids = await birthdayModel.find({serverID: server})
         if (!birthdaykids || birthdaykids.length == 0) {
             msg.channel.send(`\`No birthdays have been registered on this server. To do so, please do uwu birthday date month [eg. uwu birthday 17 march]\``)
