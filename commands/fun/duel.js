17cbe59c360251c216590ab1674982547e7bc9df
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-21 12:34:08 +0800

    Update duel.js

--- commands/fun/duel.js
+++ commands/fun/duel.js
@@ -2,6 +2,10 @@ const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
 
+    function sleep(sec) {
+        return new Promise(resolve => setTimeout(resolve, sec*1000));
+    }
+    
 	var results = [
         'Herbifors', 
         'Incendio', 
@@ -55,33 +59,44 @@ module.exports.run = async(client, msg, args) => {
             msg.channel.send(`${taggedUser}, ${msg.guild.members.cache.get(msg.author.id).displayName} wants to duel you. Do you accept? (Reply yes or no.)`);
             msg.channel.awaitMessages(m => m.author.id == taggedUser.id, {max: 1, time: 30000}).then(collected => {
                 if (collected.first().content.toLowerCase() == 'yes') {
-                    msg.channel.send("Let the battle begin!");
+                    try {
+                        await msg.channel.send(`One message`)
+                        //delay here
+                        await sleep(2);
+                        await  msg.channel.send(`One message`)
+                        //delay here
+                        await sleep(2);
+                        await  msg.channel.send(`One message`)
+                        //delay here
+                        await sleep(2);
+                      } catch (error) {
+                       console.error(error)
+                      }
+/*                     msg.channel.send("Let the battle begin!");
                     p1points = 100;
                     p2points = 100;
                     player1 = msg.guild.members.cache.get(msg.author.id).displayName;
                     player2 = taggedUser.displayName;
                     msg.channel.send(`**${player1}**: ${p1points}, **${player2}**: ${p2points}`);
+                    //delay here
                     while (p1points > 0 && p2points > 0) {
                         const randomOption = results[Math.floor(Math.random() * results.length)];
-                        setTimeout(msg.channel.send(`**${player1}** casted ${randomOption} (∩•̀ω•́)⊃-⋆`), 1000)
-                        
-                        setTimeout(results, 1000)
-                        function results() {
-                            dice = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
-                            if (dice >= 10) {
-                                hp = 50;
-                                msg.channel.send(`**${player1}**'s spell hit **${player2}**! BAM :boom: **(-${hp} HP)** `);
-                            }
-                            else if (dice > 6 && dice < 10) {
-                                hp = 20;
-                                msg.channel.send(`**${player1}**'s spell hit **${player2}**! BAM :boom: **(-${hp} HP)** `);
-                            }
-                            else if (dice <= 6) {
-                                hp = 0;
-                                msg.channel.send(`**${player1}**'s spell was blocked by **${player2}**! :shield: **(-${hp} HP)** `);
-                            }
-                        }
+                        msg.channel.send(`**${player1}** casted ${randomOption} (∩•̀ω•́)⊃-⋆`);
+                        //delay here
 
+                        dice = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
+                        if (dice >= 10) {
+                            hp = 50;
+                            msg.channel.send(`**${player1}**'s spell hit **${player2}**! BAM :boom: **(-${hp} HP)** `);
+                        }
+                        else if (dice > 6 && dice < 10) {
+                            hp = 20;
+                            msg.channel.send(`**${player1}**'s spell hit **${player2}**! BAM :boom: **(-${hp} HP)** `);
+                        }
+                        else if (dice <= 6) {
+                            hp = 0;
+                            msg.channel.send(`**${player1}**'s spell was blocked by **${player2}**! :shield: **(-${hp} HP)** `);
+                        }
                         p2points = p2points - hp; 
                         
                         if (p1points < 0) {
@@ -91,34 +106,31 @@ module.exports.run = async(client, msg, args) => {
                         if (p2points < 0) {
                             p2points = 0
                         }
-                        setTimeout(msg.channel.send("`" + player1 + ": " + p1points + ", " + player2 + ": " + p2points + "`"), 1000)
+                        //delay here
+                        msg.channel.send("`" + player1 + ": " + p1points + ", " + player2 + ": " + p2points + "`");
                         
                         if (p1points <= 0 || p2points <= 0) {
                             continue;
                         }
                         
                         const randomOption2 = results[Math.floor(Math.random() * results.length)];
-                        
-                        setTimeout(msg.channel.send(`**${player2}** casted ${randomOption2} ⋆-⊂(•̀ω•́∩)`), 1000)
-                        
-                        setTimeout(results2, 1000)
+                        //delay here
+                        msg.channel.send(`**${player2}** casted ${randomOption2} ⋆-⊂(•̀ω•́∩)`);
+                        //delay here
 
-                        function results2() {
-                            dice2 = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
-                            if (dice2 >= 10) {
-                                hp = 50;
-                                msg.channel.send(`**${player2}**'s spell hit **${player1}**! BAM :boom: **(-${hp} HP)** `);
-                            }
-                            else if (dice2 > 6 && dice2 < 10) {
-                                hp = 20;
-                                msg.channel.send(`**${player2}**'s spell hit **${player1}**! BAM :boom: **(-${hp} HP)** `);
-                            }
-                            else if (dice2 <= 6) {
-                                hp = 0;
-                                msg.channel.send(`**${player2}**'s spell was blocked by **${player1}**! :shield: **(-${hp} HP)** `);
-                            }
+                        dice2 = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
+                        if (dice2 >= 10) {
+                            hp = 50;
+                            msg.channel.send(`**${player2}**'s spell hit **${player1}**! BAM :boom: **(-${hp} HP)** `);
                         }
-
+                        else if (dice2 > 6 && dice2 < 10) {
+                            hp = 20;
+                            msg.channel.send(`**${player2}**'s spell hit **${player1}**! BAM :boom: **(-${hp} HP)** `);
+                        }
+                        else if (dice2 <= 6) {
+                            hp = 0;
+                            msg.channel.send(`**${player2}**'s spell was blocked by **${player1}**! :shield: **(-${hp} HP)** `);
+                        }
                         p1points = p1points - hp; 
                         
                         if (p1points < 0) {
@@ -128,8 +140,8 @@ module.exports.run = async(client, msg, args) => {
                         if (p2points < 0) {
                             p2points = 0
                         }
-                        setTimeout(msg.channel.send("`" + player1 + ": " + p1points + ", " + player2 + ": " + p2points + "`"), 1000)
-                        
+                        //delay here
+                        msg.channel.send("`" + player1 + ": " + p1points + ", " + player2 + ": " + p2points + "`");
                     }
                     
                     if (p1points <= 0 && p2points > 0) {
@@ -147,7 +159,7 @@ module.exports.run = async(client, msg, args) => {
                         .setTitle(`${player1} won the duel!`)
                         .setImage('https://i.gifer.com/WKta.gif');
                         msg.channel.send(embed);
-                    }
+                    } */
                 }
 
                 else if (collected.first().content.toLowerCase() == 'no') {
