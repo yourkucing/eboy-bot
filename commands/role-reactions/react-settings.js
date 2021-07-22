5c13cd84044a913664ffb64476b1eecbaaab539b
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-22 13:54:32 +0800

    Update react-settings.js

--- commands/role-reactions/react-settings.js
+++ commands/role-reactions/react-settings.js
@@ -14,80 +14,28 @@ module.exports.run = async(client, msg, args) => {
             const channels = collected.first().mentions.channels.map(channel => {
                 return channel.id;
             })
-            channel = channels[0]
-            msg.channel.send(`Please tag the role that you want: (Eg. @potatokids)\nReply with "exit" to exit.`)
-            msg.channel.awaitMessages(m => m.author.id == author, {max: 1}).then(collected => {
-                if (collected.first().content.toLowerCase() == 'exit') {
-                    msg.channel.send("Goodbye for now!")
-                    return
+            channel = msg.guild.channels.cache.get(channels[0])
+            sendingmessage = `Please click on the following reactions to get your roles:\n\n`
+            msg.channel.send(`Please tag the role that you want as well as the emoji: (Eg. @potatokids :heart:)\nKeep replying continuously and when you're done, reply with "done".`)
+            const collector = msg.channel.createMessageCollector(
+                m => m.author.id == author
+              );
+            role = []
+            reaction = []
+            for await (const m of collector) {
+                if (m.content.toLowerCase() == "done") {
+                    collector.stop()
+                    msg.channel.send(`Oh, we're done? Goodbye!`)
                 }
                 else {
-                    const roles = collected.first().mentions.roles.map(role => {
-                        return role.id;
+                    const roles = m.content.mentions.roles.map(role => {
+                        return role.id
                     })
-                    role = []
                     role.push(roles[0])
-                    msg.channel.send(`Please reply with the emoji that you want: (Eg. :heart:)\nReply with "exit" to exit.`)
-                    msg.channel.awaitMessages(m => m.author.id == author, {max: 1}).then(collected => {
-                        if (collected.first().content.toLowerCase() == 'exit') {
-                            msg.channel.send("Goodbye for now!")
-                            return
-                        }
-                        else {
-                            emoji = []
-                            emoji.push(collected.first().content.toLowerCase())
-                            msg.channel.send(`Do you want to add one more reaction? Reply with "yes" or "no". If you reply with "exit", it will exit without saving.`)
-                            msg.channel.awaitMessages(m => m.author.id == author, {max: 1}).then(collected => {
-                                if (collected.first().content.toLowerCase() == 'exit') {
-                                    msg.channel.send("Goodbye for now!")
-                                    return
-                                }
-                                else {
-                                    reply = collected.first().content.toLowerCase()
-                                    while (reply != "no") {
-                                        msg.channel.send(`Please tag the role that you want: (Eg. @potatokids)\nReply with "exit" to exit.`)
-                                        msg.channel.awaitMessages(m => m.author.id == author, {max: 1}).then(collected => {
-                                            if (collected.first().content.toLowerCase() == 'exit') {
-                                                msg.channel.send("Goodbye for now!")
-                                                return
-                                            }
-                                            else {
-                                                const roles = collected.first().mentions.roles.map(role => {
-                                                    return role.id;
-                                                })
-                                                role.push(roles[0])
-                                                msg.channel.send(`Please reply with the emoji that you want: (Eg. :heart:)\nReply with "exit" to exit.`)
-                                                msg.channel.awaitMessages(m => m.author.id == author, {max: 1}).then(collected => {
-                                                    if (collected.first().content.toLowerCase() == 'exit') {
-                                                        msg.channel.send("Goodbye for now!")
-                                                        return
-                                                    }
-                                                    else {
-                                                        emoji.push(collected.first().content.toLowerCase())
-                                                        msg.channel.send(`Do you want to add one more reaction? Reply with "yes" or "no". If you reply with "exit", it will exit without saving.`)
-                                                        msg.channel.awaitMessages(m => m.author.id == author, {max: 1}).then(collected => {
-                                                            if (collected.first().content.toLowerCase() == 'exit') {
-                                                                msg.channel.send("Goodbye for now!")
-                                                                return
-                                                            }
-                                                            else {
-                                                                reply = collected.first().content.toLowerCase
-                                                            }
-                                                        })
-                                                    }
-                                                })
-                                            }
-                                        })
-                                    }
-                                    console.log(role)
-                                    console.log(emoji)
-                                }
-                            })
-                        }
-                    })
+                    rr = m.content.split(" ")
+                    console.log(rr)
                 }
-            })
-            
+            }
         }
     })
 }
