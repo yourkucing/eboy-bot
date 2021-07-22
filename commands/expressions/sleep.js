9a42c5a0e615263725700b9839f0f64eac478973
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-22 21:08:38 +0800

    changes

--- commands/expressions/sleep.js
+++ commands/expressions/sleep.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
-	eboylog = msg.guild.channels.cache.get('867744429657292810')
+    eboylog = client.channels.cache.get('867744429657292810')
 	author = msg.author
 	eboylog.send(`**${author.username}** [${author.id}] used the **sleep** command.`)
 
