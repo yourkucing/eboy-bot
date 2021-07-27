abf01d76447ea5f7f963bfbc8439c681dd78e812
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-28 01:25:17 +0800

    Create test.js

---
+++ commands/admin/test.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+module.exports.run = async(client, msg, args) => {
+    if (msg.author.id == "279101053750870017") {
+        channelID = "719493404190572604"
+        guildID = "719491348713046057"
+        messageID = "869630102294626374"
+        channel = client.channels.cache.get(channelID)
+        message = channel.messages.fetch("701574160211771462")
+  
+        message.edit('This is my new content!')
+        .then(msg => console.log(`Updated the content of a message to ${msg.content}`))
+        .catch(console.error);
+        message.react('🦀')
+    }
+    else {
+        msg.channel.send("You don't have permission to use this, only my owner does.")
+    }
+    
+}
