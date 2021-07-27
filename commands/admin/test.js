b96177dc82e4acd7c5715b4ffbc29f413b232c43
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-28 02:17:48 +0800

    Update test.js

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -6,11 +6,11 @@ module.exports.run = async(client, msg, args) => {
         guildID = "865263454666489866"
         messageID = "869626800400121916"
         guild = client.guilds.cache.get(guildID)
-        role = guild.roles.cache.get("867909386440433685")
+        role = guild.roles.cache.get("867909438768558091")
         channel = client.channels.cache.get(channelID)
         const m = await channel.messages.fetch(messageID)
 
-        let hehim = m.reactions.cache.find(emoji => emoji.name == "🐧")
+        let hehim = m.reactions.cache.find(emoji => emoji.name == "🦑")
         m.reactions.cache.map(async (reaction) => {
             if (reaction.emoji.name !== "🐧") return;
             let reactedUsers = await reaction.users.fetch();
@@ -18,7 +18,7 @@ module.exports.run = async(client, msg, args) => {
                 if (!user.bot) {
                     users = guild.members.cache.get(user.id)
                     users.roles.add(role)
-                    console.log(`added to ${users.username}`)
+                    console.log(`added to ${user.username}`)
                 }
             })
 
