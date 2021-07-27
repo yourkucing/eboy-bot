19c43c1dc6578af9cb4aba782daf028821525e09
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-28 01:42:45 +0800

    Update test.js

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
     if (msg.author.id == "279101053750870017") {
-        channelID = "719493404190572604"
-        guildID = "719491348713046057"
-        messageID = "869630102294626374"
+        channelID = "865269025017954335"
+        guildID = "865263454666489866"
+        messageID = "869626800400121916"
         channel = client.channels.cache.get(channelID)
-        channel.messages.fetch("869630102294626374").then(m => {
-            msg.channel.send(m.content)
-            m.edit('hi')
-            m.react('🦀')
+        channel.messages.fetch(messageID).then(m => {
+            console.log(m.content)
+            // m.edit('hi')
+            // m.react('🦀')
         })
     }
     else {
