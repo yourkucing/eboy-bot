bea4e5917c1a0c410a0d2c81c3a2e4f9a9d08bb7
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-28 02:14:16 +0800

    Update test.js

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -9,21 +9,20 @@ module.exports.run = async(client, msg, args) => {
         role = guild.roles.cache.get("867909386440433685")
         channel = client.channels.cache.get(channelID)
         const m = await channel.messages.fetch(messageID)
-        console.log(m.content)
 
         let hehim = m.reactions.cache.find(emoji => emoji.name == "🐧")
         m.reactions.cache.map(async (reaction) => {
             if (reaction.emoji.name !== "🐧") return;
             let reactedUsers = await reaction.users.fetch();
-            console.log(reactedUsers)
             reactedUsers.map((user) => {
-                user.roles.add(role)
-                console.log(`added to ${users.username}`)
+                if (!user.bot) {
+                    users = guild.members.cache.get(user.id)
+                    users.roles.add(role)
+                    console.log(`added to ${users.username}`)
+                }
             })
 
         })
-        m.edit(`Please click on the following reactions to get your roles:\n\n🐌 for <@&867909305206308874>\n🐧 for <@&867909386440433685>\n🦑 for <@&867909438768558091>\n🦀 for <@&867909742968836146>`)
-        m.react('🦀')
     }
     else {
         msg.channel.send("You don't have permission to use this, only my owner does.")
