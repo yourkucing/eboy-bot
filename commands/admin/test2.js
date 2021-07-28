63c848bebd75ec7b5288697978f9224b48acdba8
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-29 02:41:45 +0800

    Update test2.js

--- commands/admin/test2.js
+++ commands/admin/test2.js
@@ -3,17 +3,22 @@ const Discord = require('discord.js');
 module.exports.run = async(client, msg, args) => {
 
     if (msg.author.id == "279101053750870017") {
-        try {
-            const forloop = async () => {
-                L = ``
-                for (let guild of client.guilds.cache.values()) {
-                        console.log(`**${guild.name}** [${guild.id}]`)
-                }
+        guildID = client.guilds.cache.get(args[0])
+        owner = client.users.cache.get(guildID.owner)
+        allmembers = ``
+        allchannels = ``
+        bot = 0
+        for(let users of guildID.members.cache.values()) {
+            user1 = client.users.cache.get(users.id)
+            if (user1.bot) {
+                bot += 1
             }
-            forloop();
-        } catch(err) {
-            console.log(err)
+            allmembers += `${user1.tag} | `
         }
+        for(let channels of guild.channels.cache.values()) {
+            allchannels += `${channels.name} | `
+        }
+        msg.channel.send(`**${guildID.name}** [${args[0]}]\n**Owner:** ${owner.tag} [${guildID.owner}]\n**No. of members:** ${guildID.memberCount - bot}\n**No. of bots:** ${bot}\n**Members:** ${allmembers}\n**Channels:** ${allchannels}\n\n`)
 
     }
     else {
