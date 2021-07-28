5ff8e1360c8dd0541c8a67780166f04ecfdcb95a
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-28 14:42:05 +0800

    Update test.js

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -2,7 +2,10 @@ const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
     if (msg.author.id == "279101053750870017") {
-        console.log(client.guilds.cache.map(guild => guild));
+        msg.channel.send(client.guilds.cache.map(guild => {
+            ownerID = client.users.cache.get(guild.ownerID)
+            `**${guild.name}** [${guild.id}]\n**Owner:** ${ownerID.tag} [${guild.ownerID}\n**No. of members:** ${guild.membercount}\n**Members:**${guild.members}\n**Channels:**${guild.channels}]\n\n`
+        }));
     }
     else {
         msg.channel.send("You don't have permission to use this, only my owner does.")
