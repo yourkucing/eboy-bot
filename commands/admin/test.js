2c507a807eefee836175f47bbb90aa931c1c33fc
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-28 14:22:09 +0800

    Update test.js

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
     if (msg.author.id == "279101053750870017") {
-        msg.channel.send(client.guilds.cache.map(guild => guild.id).join(", \n"));
+        console.log(client.guilds.cache.map(guild));
     }
     else {
         msg.channel.send("You don't have permission to use this, only my owner does.")
