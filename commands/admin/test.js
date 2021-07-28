ad65b626f3deef084fe810bb4f96bf7e06478c9a
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-28 14:46:32 +0800

    Update test.js

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async(client, msg, args) => {
     if (msg.author.id == "279101053750870017") {
         msg.channel.send(client.guilds.cache.map(guild => {
-            ownerID = client.users.cache.get(guild.ownerID)
+            ownerID = (client.guilds.cache.get(guild.id)).members.cache.get(guild.ownerID)
             `**${guild.name}** [${guild.id}]\n**Owner:** ${ownerID.tag} [${guild.ownerID}\n**No. of members:** ${guild.membercount}\n**Members:**${guild.members}\n**Channels:**${guild.channels}]\n\n`
         }));
     }
