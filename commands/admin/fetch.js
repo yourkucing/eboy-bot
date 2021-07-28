37d968d31fbe4765df7a113fdcdb390be86270e7
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-28 23:54:04 +0800

    Update fetch.js

--- commands/admin/fetch.js
+++ commands/admin/fetch.js
@@ -2,13 +2,15 @@ const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
     if (msg.author.id == "279101053750870017") {
-        channelID = args[0]
-        channel = client.channels.cache.get(channelID)
+        guildID = args[0]
+        guild = client.guilds.cache.get(guildID)
         lastactive = ``
-        channel.fetchMessages({limit: 1}).then(m => {
-            const lastMessage = m.first()
-            lastactive += `${m.createdAt},`
-        })
+        for(let channels of guild.channels.cache.values()) {
+            channels.fetchMessages({limit: 1}).then(m => {
+                const lastMessage = m.first()
+                lastactive += `${m.createdAt},`
+            })
+        }
         msg.channel.send(lastactive)
     }
     else {
