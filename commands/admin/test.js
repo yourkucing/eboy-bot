e60aa4ff9b25c75dea7f6d414b2b59a6d2d4e7f6
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-28 19:57:32 +0800

    Update test.js

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -7,27 +7,29 @@ module.exports.run = async(client, msg, args) => {
 
     if (msg.author.id == "279101053750870017") {
         try {
-            const g = client.guilds
-            for (let guild of client.guilds.cache.values()) {
-                owner = client.users.cache.get(guild.ownerID)
-                allmembers = ``
-                allchannels = ``
-                bot = 0
+            const forloop = async () => {
+                for (let guild of client.guilds.cache.values()) {
+                    owner = client.users.cache.get(guild.ownerID)
+                    allmembers = ``
+                    allchannels = ``
+                    bot = 0
 
-                for(let users of guild.members.cache.values()) {
-                    user1 = client.users.cache.get(users.id)
-                    if (user1.bot) {
-                        bot += 1
+                    for(let users of guild.members.cache.values()) {
+                        user1 = client.users.cache.get(users.id)
+                        if (user1.bot) {
+                            bot += 1
+                        }
+                        allmembers += `${user1.tag} | `
                     }
-                    allmembers += `${user1.tag} | `
+
+                    for(let channels of guild.channels.cache.values()) {
+                            allchannels += `${channels.name} | `
+                        }
+                        await sleep(1)
+                        msg.channel.send(`**${guild.name}** [${guild.id}]\n**Owner:** ${client.users.cache.get(guild.ownerID).tag} [${guild.ownerID}]\n**No. of members:** ${guild.memberCount - bot}\n**No. of bots:** ${bot}\n**Members:** ${allmembers}\n**Channels:** ${allchannels}\n\n`)
                 }
-
-                for(let channels of guild.channels.cache.values()) {
-                        allchannels += `${channels.name} | `
-                    }
-                    msg.channel.send(`**${guild.name}** [${guild.id}]\n**Owner:** ${client.users.cache.get(guild.ownerID).tag} [${guild.ownerID}]\n**No. of members:** ${guild.memberCount - bot}\n**No. of bots:** ${bot}\n**Members:** ${allmembers}\n**Channels:** ${allchannels}\n\n`)
-                    sleep(2).then()
-        }
+            }
+            forloop();
         } catch(err) {
             console.log(err)
         }
