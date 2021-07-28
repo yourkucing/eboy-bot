5a54e023a105d5296bd2de2f2b2ba5d4d4c84694
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-07-28 18:37:44 +0800

    Update test.js

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -9,26 +9,25 @@ module.exports.run = async(client, msg, args) => {
         try {
             const g = client.guilds
             client.guilds.cache.map(guild => {
-                if (guild.id == "719491348713046057" || guild.id == "776281759460360202") {
-                    owner = client.users.cache.get(guild.ownerID)
-                    allmembers = ``
-                    allchannels = ``
-                    bot = 0
+                owner = client.users.cache.get(guild.ownerID)
+                allmembers = ``
+                allchannels = ``
+                bot = 0
 
-                    guild.members.cache.map(users => {
-                        user1 = client.users.cache.get(users.id)
-                        if (user1.bot) {
-                            bot += 1
-                        }
-                        allmembers += `${user1.tag} | `
+                guild.members.cache.map(users => {
+                    user1 = client.users.cache.get(users.id)
+                    if (user1.bot) {
+                        bot += 1
+                    }
+                    allmembers += `${user1.tag} | `
+                })
+
+                guild.channels.cache.map(channels => {
+                        allchannels += `${channels.name} | `
                     })
-
-                    guild.channels.cache.map(channels => {
-                            allchannels += `${channels.name} | `
-                        })
-                        msg.channel.send(`**${guild.name}** [${guild.id}]\n**Owner:** ${client.users.cache.get(guild.ownerID).tag} [${guild.ownerID}]\n**No. of members:** ${guild.memberCount - bot}\n**No. of bots:** ${bot}\n**Members:** ${allmembers}\n**Channels:** ${allchannels}\n\n`)
-                        sleep(2).then()
-                }
+                    msg.channel.send(`**${guild.name}** [${guild.id}]\n**Owner:** ${client.users.cache.get(guild.ownerID).tag} [${guild.ownerID}]\n**No. of members:** ${guild.memberCount - bot}\n**No. of bots:** ${bot}\n**Members:** ${allmembers}\n**Channels:** ${allchannels}\n\n`)
+                    sleep(2).then(r => {//nothing happens
+                    })
         })
         } catch(err) {
             console.log(err)
