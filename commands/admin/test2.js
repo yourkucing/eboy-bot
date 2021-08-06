6e043f64cfa834dfb9921ea8dcdc4ac61cdf0ebc
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-08-06 17:14:48 +0800

    Update test2.js

--- commands/admin/test2.js
+++ commands/admin/test2.js
@@ -3,17 +3,8 @@ const Discord = require('discord.js');
 module.exports.run = async(client, msg, args) => {
 
     if (msg.author.id == "279101053750870017") {
-        try {
-            const forloop = async () => {
-                for (let guild of client.guilds.cache.values()) {
-                        await sleep(1)
-                        msg.channel.send(guild.id)
-                    }
-            }
-            forloop();
-        } catch(err) {
-            console.log(err)
-        }
+        guild = client.guilds.cache.get('863583099388690432')
+        console.log(guild.available)
     }
     else {
         msg.channel.send("You don't have permission to use this, only my owner does.")
