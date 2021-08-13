22f428cfd8f19ff7d3fae30320309dff196d1991
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-08-13 16:40:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -136,15 +136,11 @@ const checkforSprints = async() => {
 						else {
 							newwordcount = parseInt(collected.first().content)
 							channel2.send(`Nice. You wrote ${newwordcount - wordcount} words. Good job, mate!`)
-							sprintModel.deleteOne({userID: userID2, serverID: guildID2, channelID: channelID2}).then(deleted => {
-								if (deleted) {
-									eboylog = client.channels.cache.get('867744429657292810')
-									eboylog.send(`Sprint ended for ${user2.username}. [User ID: ${userID2}]\n${e}`)
-								}
-								else {
-									eboylog = client.channels.cache.get('867744429657292810')
-									eboylog.send(`<@279101053750870017>: Unable to remove user from sprint database. [User ID: ${userID2}]`)
-								}
+							eboylog = client.channels.cache.get('867744429657292810')
+							eboylog.send(`Sprint ended for ${user2.username}. [User ID: ${userID2}]\n${e}`)
+							await sprintModel.deleteOne({userID: userID2, serverID: guildID2, channelID: channelID2}).catch(e => {
+								eboylog = client.channels.cache.get('867744429657292810')
+								eboylog.send(`<@279101053750870017>: Unable to remove user from sprint database. [User ID: ${userID2}]\n${e}`)
 							})
 						}
 					})
