a9e6929a367eebd204c1f9328b38eaa41df37bdd
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-12-09 16:42:20 +0800

    changes

--- commands/decisions/sprint.js
+++ commands/decisions/sprint.js
@@ -19,17 +19,16 @@ module.exports.run = async(client, msg, args) => {
         const filter = m => m.author.id === sprinter;
         msg.channel.send(`Please key in your original word count: ("Exit" to exit. If you have no word count, just key in "0".)`);
         msg.channel.awaitMessages({filter, max: 1}).then(collected => {
-            console.log(collected)
-/*             if (collected.first().content.toLowerCase() == 'exit') {
+            if (collected.content.toLowerCase() == 'exit') {
                 msg.channel.send("Goodbye for now!");
                 return
             }
-            else if (isNaN(parseInt(collected.first().content))) {
+            else if (isNaN(parseInt(collected.content))) {
                 msg.channel.send("That's not a number, bro. Please retry.")
                 return
             }
             else {
-                wordcount = parseInt(collected.first().content)
+                wordcount = parseInt(collected.content)
                 msg.channel.send(`Writing sprint for ${args} minutes starts now, <@${sprinter}>!`)
                 sprint = sprintModel.create({
                     userID: sprinter,
@@ -38,7 +37,7 @@ module.exports.run = async(client, msg, args) => {
                     sprint: Date.now() + time,
                     word: wordcount
                 }).catch((e) => { console.log(e); })
-            } */
+            }
         })
         
     }
--- commands/expressions/angry.js
+++ commands/expressions/angry.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const { Client, Intents, MessageEmbed, Permissions } = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
     eboylog = client.channels.cache.get('867744429657292810')
@@ -16,10 +16,10 @@ module.exports.run = async(client, msg, args) => {
 		]
 	var randomgif = gifs[Math.floor(Math.random()*gifs.length)];
 	const taggedUser = msg.mentions.users.first();
-	const embed = new Discord.MessageEmbed()
+	const embed = new MessageEmbed()
 	.setColor('#FF69B4')
 	.setDescription(`yikes, ${msg.guild.members.cache.get(msg.author.id).displayName} is angry!`)
 	.setImage(randomgif)
 	.setFooter(`Everyone, back away!`);
-	msg.channel.send(embed);
+	msg.channel.send({embeds: [embed]});
 }
