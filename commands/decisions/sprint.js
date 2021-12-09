5bb0cdac17dec0fe4b473f9398d353a200ad1497
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-12-09 16:24:49 +0800

    Update sprint.js

--- commands/decisions/sprint.js
+++ commands/decisions/sprint.js
@@ -18,9 +18,10 @@ module.exports.run = async(client, msg, args) => {
     else {
         msg.channel.send(`Please key in your original word count: ("Exit" to exit. If you have no word count, just key in "0".)`);
         const filter = m => {
-            m.author.id == sprinter
+            m.author.id === sprinter
         }
         msg.channel.awaitMessages({filter, max: 1}).then(collected => {
+            console.log(collected)
             if (collected.first().content.toLowerCase() == 'exit') {
                 msg.channel.send("Goodbye for now!");
                 return
@@ -30,6 +31,7 @@ module.exports.run = async(client, msg, args) => {
                 return
             }
             else {
+                msg.channel.send(collected.first().content)
                 wordcount = parseInt(collected.first().content)
                 msg.channel.send(`Writing sprint for ${args} minutes starts now, <@${sprinter}>!`)
                 sprint = sprintModel.create({
