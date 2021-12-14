5392c34e7af91192e9e632bd493f816dba619e11
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-12-14 15:18:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ const checkforTimeouts = async() => {
 const checkforBirthdays = async() => {
 	day = new Date().getDate()
 	month = new Date().getMonth() + 1
-	if (new Date().getUTCHours() == 14 && new Date().getUTCMinutes() == 18) {
+	if (new Date().getUTCHours() == 6 && new Date().getUTCMinutes() == 0) {
 		birthday = new Date(`2000-${month}-${day}`)
 		const query = {
 			birthday: birthday
