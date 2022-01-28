a9fac7babcf27680ed1bc41d61c95e1ee77036e0
Maryam Dinzly <thatdinzlygirl@gmail.com>
2022-01-28 21:51:15 +0800

    changes

--- commands/decisions/8ball.js
+++ commands/decisions/8ball.js
@@ -7,26 +7,26 @@ module.exports.run = async(client, msg, args) => {
     eboylog.send(`**${author.username}** [${author.id}] used the **8ball** command in **${guild}** [${msg.guild.id}].`)
 
 	var answers = [
-		'As I see it, yes.',
-        'Ask again later.',
-        'Better not tell you now.',
-        'Cannot predict now.',
-        'Concentrate and ask again.',
-        'Don’t count on it.',
-        'It is certain.',
-        'It is decidedly so.',
-        'Most likely.',
-        'My reply is no.',
-        'My sources say no.',
-        'Outlook not so good.',
-        'Outlook good.',
-        'Reply hazy, try again.',
-        'Signs point to yes.',
-        'Very doubtful.',
-        'Without a doubt.',
+		'As I see it, yes uwu. Lesgo!',
+        'Hmmm, ask again later. The stars say to shhhh for now.',
+        'Better not tell you now. What if it breaks the universe?!',
+        'The crystal ball is sleeping, so I can\'t predict now!',
+        'Concentrate REAAAAL HARD and ask again uwu.',
+        'Don\'t count on it. Like, really. Don\'t.',
+        'It is certain. HUNDRED PERCENT. YES.',
+        'It is decidedly so. Wow, I sound like a shifu.',
+        'MMmMMMMm. Most likely.',
+        'My reply is no. Why? You don\'t like it? Fight me 1v1 in the garden then!',
+        'My sources say no. I don\'t know why. They\'re always grumpy.',
+        'Outlook not so good. Maybe try inlook?',
+        'Outlook good. Everything is good.',
+        'R-r-r-rep-p-p-p-ly haz-z-z-z-y, try aga-a-a-a-ain.',
+        'Signs point to yes. Like, ALL signs. Even the STOP sign.',
+        'Very doubtful. Truly doubtful.',
+        'Without a doubt. I cannot find the doubt. Error 404.',
         'Yes.',
         'Yes – definitely.',
-        'You may rely on it.'
+        'You may rely on it. But not on me. I might not be reliable.'
 		]
     msg.channel.send(`*shakes 8ball slowly*`).then((sentMessage) => 
         setTimeout(function(){
--- commands/user-actions/untonk.js
+++ commands/user-actions/untonk.js
@@ -6,10 +6,9 @@ module.exports.run = async(client, msg, args) => {
 	guild = client.guilds.cache.get(msg.guild.id)
 	eboylog.send(`**${author.username}** [${author.id}] used the **untonk** command in **${guild}** [${msg.guild.id}].`)
 	var gifs = [
-		'https://s4.gifyu.com/images/untonk1.gif',
-		'https://s4.gifyu.com/images/untonk2.gif',
-		'https://s4.gifyu.com/images/tonk3.gif',
-		'https://s6.gifyu.com/images/tonk4.gif'
+		'https://i.gifer.com/DnDH.gif',
+		'https://i.pinimg.com/originals/47/e2/60/47e260a9aaaee0c887bbc24a8e1b2fdf.gif',
+		'https://64.media.tumblr.com/e17f4f4b48e9ba6b71fb5ed2d6c46905/tumblr_oo0w2xXN681v5i3nmo1_500.gif'
 		]
 	if (!msg.mentions.users.size) {
 		const words = args.join(' ');
