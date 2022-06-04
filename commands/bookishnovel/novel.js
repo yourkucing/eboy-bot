cd1053d7cec2ed708d5e725f412b74d33c648b01
Maryam Dinzly <thatdinzlygirl@gmail.com>
2022-06-04 18:51:40 +0200

    updated rank and level

--- commands/bookishnovel/novel.js
+++ commands/bookishnovel/novel.js
@@ -15,7 +15,9 @@ module.exports.run = async(client, msg, args) => {
         .setRequiredXP(1000)
         .setProgressBar("#FFFFFF", "COLOR")
         .setUsername(author.username)
-        .setDiscriminator(author.discriminator);
+        .setDiscriminator(author.discriminator)
+        .setRank(0, "", false)
+        .setLevel(0, "", false);
     
     rank.build()
     .then(data => {
