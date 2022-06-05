aec01b9870d81c9b4fb367f7e19eb8073cb359c3
Maryam Dinzly <thatdinzlygirl@gmail.com>
2022-06-05 16:22:41 +0200

    Update novel.js

--- commands/bookishnovel/novel.js
+++ commands/bookishnovel/novel.js
@@ -8,18 +8,8 @@ module.exports.run = async(client, msg, args) => {
     eboylog.send(`**${author.username}** [${author.id}] used the **novel** command in **${guild}** [${msg.guild.id}].`)
 
     img = "https://iconape.com/wp-content/png_logo_vector/book-18.png"
-
-    const rank = new canvacord.Rank()
-        .setAvatar(img)
-        .setCurrentXP(356)
-        .setRequiredXP(1000)
-        .setProgressBar("#FFFFFF", "COLOR")
-        .setUsername(author.username)
-        .setDiscriminator(author.discriminator)
-        .setRank(0, "", false)
-        .setLevel(0, "", false);
     
-    rank.build()
+    canvacord.createProgressBar()
     .then(data => {
         const attachment = new MessageAttachment(data, "RankCard.png")
         const embed = new MessageEmbed()
