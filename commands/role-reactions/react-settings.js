b22df847afc5fcd6c96f153afea0d7b65fa316c6
Maryam Dinzly <thatdinzlygirl@gmail.com>
2022-06-08 20:34:02 +0200

    Update react-settings.js

--- commands/role-reactions/react-settings.js
+++ commands/role-reactions/react-settings.js
@@ -28,58 +28,65 @@ module.exports.run = async(client, msg, args) => {
                 })
                 channel = msg.guild.channels.cache.get(channels[0])
                 sendingmessage = `Please click on the following reactions to get your roles:\n`
-                msg.channel.send(`Please tag the role that you want as well as the emoji: (Eg. @potatokids :heart:)\nKeep replying continuously and when you're done, reply with "done".`)
+                msg.channel.send(`Please key in the role:`)
                 const filter = m => m.author.id == author;
-                const collector = msg.channel.createMessageCollector({filter}
-                    );
+                msg.channel.awaitMessages({filter, max: 1}).then(collected => {
+                    rr = collected.first.content
+                    console.log(rr)
+                }).catch(collected => {
+                    
+                    });
+                // const filter = m => m.author.id == author;
+                // const collector = msg.channel.createMessageCollector({filter}
+                //     );
         
-                role = []
-                reaction = []
-                for await (const m of collector) {
-                    if (m.content.toLowerCase() == "done") {
-                        collector.stop()
-                        msg.channel.send(`Oh, we're done? Goodbye!`)
-                    }
-                    else {
-                        const roles = m.mentions.roles.map(role => {
-                            return role.id
-                        })
-                        role.push(roles[0])
-                        rr = m.content.split(" ")
-                        sendingmessage += `\n${rr[1]} for <@&${roles[0]}>`
-                        if(rr[1].includes("<") && rr[1].includes(">") && rr[1].includes(":")){
-                            rr = rr[1].replace(/\s*\:.*?\:\s*/g, "")
-                            rr = rr.replace("<", "")
-                            rr = rr.replace(">", "")
-                            reactionEmoji = rr
-                        }
-                        else {
-                            reactionEmoji = rr[1]
-                        }
-                        reaction.push(reactionEmoji)
+                // role = []
+                // reaction = []
+                // for await (const m of collector) {
+                //     if (m.content.toLowerCase() == "done") {
+                //         collector.stop()
+                //         msg.channel.send(`Oh, we're done? Goodbye!`)
+                //     }
+                //     else {
+                //         const roles = m.mentions.roles.map(role => {
+                //             return role.id
+                //         })
+                //         role.push(roles[0])
+                //         rr = m.content.split(" ")
+                //         sendingmessage += `\n${rr[1]} for <@&${roles[0]}>`
+                //         if(rr[1].includes("<") && rr[1].includes(">") && rr[1].includes(":")){
+                //             rr = rr[1].replace(/\s*\:.*?\:\s*/g, "")
+                //             rr = rr.replace("<", "")
+                //             rr = rr.replace(">", "")
+                //             reactionEmoji = rr
+                //         }
+                //         else {
+                //             reactionEmoji = rr[1]
+                //         }
+                //         reaction.push(reactionEmoji)
     
-                    }
-                }
-                channel.send(sendingmessage)
-                .then(async function(msg1) {
-                    try {
-                        for (let i = 0; i < reaction.length; i++) {
-                            await msg1.react(reaction[i])
-                            reactionsModel.create({
-                                serverID: serverID,
-                                channelID: channels[0],
-                                messageID: msg1.id,
-                                emoji: reaction[i],
-                                role: role[i]
-                            }).catch(e => {console.log(e)})
-                        }
-                    }
-                    catch(err) {
-                        msg.channel.send(`\`There is an error in reacting to the message. Please delete the message that eboy sent and try again. If error persists, contact Maryam#9206.\``)
-                        console.log(err)
-                    }   
-                })
-            }
-        }
+                //     }
+                // }
+                // channel.send(sendingmessage)
+                // .then(async function(msg1) {
+                //     try {
+                //         for (let i = 0; i < reaction.length; i++) {
+                //             await msg1.react(reaction[i])
+                //             reactionsModel.create({
+                //                 serverID: serverID,
+                //                 channelID: channels[0],
+                //                 messageID: msg1.id,
+                //                 emoji: reaction[i],
+                //                 role: role[i]
+                //             }).catch(e => {console.log(e)})
+                //         }
+                //     }
+                //     catch(err) {
+                //         msg.channel.send(`\`There is an error in reacting to the message. Please delete the message that eboy sent and try again. If error persists, contact Maryam#9206.\``)
+                //         console.log(err)
+                //     }   
+        //         })
+             }
+         }
     }
 }
