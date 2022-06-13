d08f01dcf3e72513dff0c467266daa57e2d648f8
Maryam Dinzly <thatdinzlygirl@gmail.com>
2022-06-13 23:19:51 +0200

    Update react-settings.js

--- commands/role-reactions/react-settings.js
+++ commands/role-reactions/react-settings.js
@@ -45,15 +45,76 @@ module.exports.run = async(client, msg, args) => {
                                 .setDescription(`Please react to one of the emojis to get the role.\nClick the reaction again to the role.`);
                             if (!collected1.first().emoji.id) {
                                 react = collected1.first().emoji.name
-                                sendingmessage += `\n${react}: <@&${roleID}>`
+                                sendingmessage += `\n${react} for <@&${roleID}>`
                                 embed.addField({name: `Role Reactions`, value: sendingmessage})
                             }
                             else {
                                 react = collected1.first().emoji.id
-                                sendingmessage += `\n<:${collected1.first().emoji.name}:${react}>: <@&${roleID}>`
+                                sendingmessage += `\n<:${collected1.first().emoji.name}:${react}> for <@&${roleID}>`
                                 embed.addFields({name: `Role Reactions`, value: sendingmessage})
                             }
-                            chan.send({embeds: [embed]})
+                            chan.send({embeds: [embed]}).then(msg1 => {
+                                msg1.react(react)
+                                msgID = msg1.id
+                                msg.channel.send(`Do you want to add another role reaction? Reply \`yes\` or \`no\`.`).then(reply1 => {
+                                    num = 0
+                                    while (num == 0) {
+                                        const filter = m => m.author.id == author
+                                        msg.channel.awaitMessages({filter, max: 1}).then(collected => {
+                                            if (collected.first().content.toLowerCase() == "no") {
+                                                num == 1
+                                            }
+                                            else if (collected.first().content.toLowerCase() == "yes") {
+                                                num == 0
+                                                msg.channel.send(`Please key in the role:`)
+                                                msg.channel.awaitMessages({filter, max: 1}).then(collected => {
+                                                    rr = collected.first().mentions.roles.map(role => {
+                                                        return role.id
+                                                    })
+                                                    roleID = rr[0]
+                                                    msg.channel.send(`Please **react** to this message for the emoji that you want to use with the role stated above.`)
+                                                    .then(message => {
+                                                        const filter = (reaction, user) => {return reaction.emoji.name && user.id == author}
+                                                        message.awaitReactions({filter, max: 1}).then(collected1 => {
+                                                            if (!collected1.first().emoji.id) {
+                                                                react = collected1.first().emoji.name
+                                                                sendingmessage += `\n${react} for <@&${roleID}>`
+                                                                chan.messages.fetch(msgID).then(r => {
+                                                                    const embed = new MessageEmbed()
+                                                                        .setColor(`#FF69B4`)
+                                                                        .setDescription(`Please react to one of the emojis to get the role.\nClick the reaction again to the role.`)
+                                                                        .setFields({name: `Role Reactions`, value: sendingmessage});
+                                                                    r.edit({embeds: [embed]}).then(msg1 => {
+                                                                        msg1.react(react)
+                                                                        msg.channel.send(`Do you want to add another role reaction? Reply \`yes\` or \`no\`.`)
+                                                                    })
+
+                                                                })
+                                                            }
+                                                            else {
+                                                                react = collected1.first().emoji.id
+                                                                sendingmessage += `\n<:${collected1.first().emoji.name}:${react}> for <@&${roleID}>`
+                                                                chan.messages.fetch(msgID).then(r => {
+                                                                    const embed = new MessageEmbed()
+                                                                        .setColor(`#FF69B4`)
+                                                                        .setDescription(`Please react to one of the emojis to get the role.\nClick the reaction again to the role.`)
+                                                                        .setFields({name: `Role Reactions`, value: sendingmessage});
+                                                                    r.edit({embeds: [embed]}).then(msg1 => {
+                                                                        msg1.react(react)
+                                                                        msg.channel.send(`Do you want to add another role reaction? Reply \`yes\` or \`no\`.`)
+                                                                    })
+
+                                                                })
+                                                            }
+                                                        })
+                                                    })
+                                                })
+                                            }
+                                        })
+                                    }
+                                    msg.channel.send(`Alright, goodbye uwu! :3`)
+                                })
+                            })
                         }).catch(e => {
                             console.log(e)
                         })
